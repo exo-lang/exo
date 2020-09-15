2eee91490e8ccde1123e5a85b1a766a17c23d8f0
gilbo <gilbert@gilbertbernstein.com>
2020-09-14 17:56:30 -0700

    remove generated test files from version control

--- tests/test.c
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-
-// add_vec( x : R[n] IN, y : R[n] IN, res : R[n] OUT )
-void add_vec(size_t n, float* x, float* y, float* res) {
-for (int i_5=0; i_5 < n; i_5++) {
-res[i_5] = x[i_5] + y[i_5];
-}
-}
-
--- tests/test.h
+++
@@ -1,5 +0,0 @@
-#include <cstdio>
-#include <cstring>
-
-// add_vec( x : R[n] IN, y : R[n] IN, res : R[n] OUT )
-void add_vec(size_t n, float* x, float* y, float* res);
