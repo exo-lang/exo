be2bbb4f3b806c17f9ed5463f6b93bcb3403bd45
Yuka Takahashi <yukatkh@gmail.com>
2021-01-13 13:37:57 -0500

    Add nested alloc test

--- tests/test_alloc.py
+++ tests/test_alloc.py
@@ -12,31 +12,55 @@ from SYS_ATL import proc, Procedure
 sys.path.append(sys.path[0]+"/.")
 from .helper import *
 
-"""
-def test_alloc_nest_ir():
-    TEST_3 = gen_alloc_nest_ir()
+def gen_alloc_nest():
+    @proc
+    def alloc_nest(n : size, m : size,
+                   x : R[n,m] @ IN, y: R[n,m] @ IN, res : R[n,m] @ OUT):
+        rloc : R[m]
+        for i in par(0,n):
+            xloc : R[m]
+            yloc : R[m]
+            for j in par(0,m):
+                xloc[j] = x[i,j]
+            for j in par(0,m):
+                yloc[j] = y[i,j]
+            for j in par(0,m):
+                rloc[j] = xloc[j] + yloc[j]
+            for j in par(0,m):
+                res[i,j] = rloc[j]
+
+    return alloc_nest
+
+def test_alloc_nest():
+    alloc_nest = gen_alloc_nest()
+    assert type(alloc_nest) is Procedure
+
     filename = "test_alloc_nest"
-    run_compile([TEST_3], directory, (filename + ".c"), (filename + ".h"))
-    compile_so_cmd = ("clang -Wall -Werror -fPIC -O3 -shared " +
-                      "-o " + directory + filename + ".so " +
-                      directory + filename + ".c")
-    subprocess.run(compile_so_cmd, check=True, shell=True)
-    abspath = os.path.dirname(os.path.abspath(filename))
-    test_lib = ctypes.CDLL(abspath + '/' + directory + filename + ".so")
+
+    # Write pretty printing to a file
+    f_pretty = open(os.path.join(directory, filename + "_pretty.atl"), "w")
+    f_pretty.write(str(alloc_nest))
+    f_pretty.close()
+
+    alloc_nest.compile_c(directory, filename)
+
     x = nparray([[1.0, 2.0, 3.0], [3.2, 4.0, 5.3]])
     y = nparray([[2.6, 3.7, 8.9], [1.3, 2.3, 6.7]])
     n_size = 2
     m_size = 3
     res = nprand(size=(n_size, m_size))
     res_c = cvt_c(res)
+
+    test_lib = generate_lib(directory, filename)
     test_lib.alloc_nest(c_int(n_size), c_int(
         m_size), cvt_c(x), cvt_c(y), res_c)
     res_c = np.ctypeslib.as_array(res_c, shape=(n_size, m_size))
-    Interpreter(TEST_3, n=n_size, m=m_size, x=x, y=y, res=res)
+    alloc_nest.interpret(n=n_size, m=m_size, x=x, y=y, res=res)
     np.testing.assert_almost_equal(res, res_c)
     np.testing.assert_almost_equal(res_c, nparray(
         [[3.6, 5.7, 11.9], [4.5, 6.3, 12.0]]))
-"""
+
+
 """
 @proc
 GEMM_Load(y : R[...], i : index, x : R[...], j : index, n : size):
