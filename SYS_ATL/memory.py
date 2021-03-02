351557d6fc0cc9a17bcde07feb77a2403663d324
Yuka Ikarashi <yukatkh@gmail.com>
2021-03-02 18:54:44 -0500

    memory sketch with Gilbert

--- SYS_ATL/memory.py
+++ SYS_ATL/memory.py
@@ -1,5 +1,6 @@
 from .prelude import *
 from . import shared_types as T
+from . import TL_float, TL_double, TL_int
 
 # --------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------- #
@@ -13,8 +14,7 @@ from . import shared_types as T
 """
 #   A memory consists of
 #     - a global C-code block
-#     - C-code to initialize the memory
-#     - C-code to:
+#     - C-code macros (as Python functions) to:
 #           * allocate from the memory
 #           * free an allocation
 #           * read from the memory (optional)
@@ -22,59 +22,89 @@ from . import shared_types as T
 #           * reduce to the memory (optional)
 """
 
-# This MetaData will be compiled into
-# struct GEMM_req_metadata {
-class MetaData:
-    def __init__(self,
-        row_size,
-        col_size,
-        buf_type,
-        buf_bits,
-    ):
-    
-    self.row_size = row_size
-    self.col_size = col_size
-    self.buf_type = buf_type
-    self.buf_bits = buf_bits
-
+class MemGenError(Exception):
+    pass
 
 class Memory:
     def __init__(self, name,
-        metadata= None,
-        alloc   = None,
+        global  = None, # C code
+        alloc   = None, # python gemmini_extended_compute_preloaded
         free    = None,
         read    = None,
         write   = None,
         red     = None,
     ):
-
-    self._name  = name
-    self._meta  = metadata
-    self._alloc = alloc
-    self._free  = free
-    self._read  = read
-    self._write = write
-    self._reduce= red
+        if alloc is None:
+            raise TypeError("must supply 'alloc' argument")
+        if free is None:
+            raise TypeError("must supply 'free' argument")
+        self._name      = name
+        self._global    = global
+        self._alloc     = alloc
+        self._free      = free
+        self._read      = read
+        self._write     = write
+        self._reduce    = red
 
 # TODO: How to encode size??
 # Or assume that alloc/free code must not change by types?
 # How to handle different types? e.g. float, double,..
 #
-#if s.type is T.R:
-#    self.add_line(f"float {name};")
-#else:
-#    size = _type_size(s.type, self.env)
-#    self.add_line(f"float *{name} = " +
-#            f"(float*) malloc ({size} * sizeof(float));")
 # Check this in typecheck.py, isinstance(Memory)
 # We should at least create TL_float
 
 # Write some compiler-ish code here?
-def dram_alloc(x) :
-    if type(x) is TL_float:
-        return ("float {x};")
-    elif type(x) is TL_double:
-        return ("double {x};")
+# Create a wrapper for Sym,
+
+
+mem.alloc( self.sym_to_str(s.name),
+           s.type.basetype(),
+           self.shape_strs( s.type.shape() ) )
+
+def dram_alloc(new_name, prim_type, shape, error):
+    """
+        - new_name is a string with the variable name to be allocated.
+        - prim_type is a string with the c-type of elements of the new
+          buffer: e.g. 'float', 'double', 'bfloat16', 'int32', etc.
+        - shape is a python tuple containing positive integers and/or
+          strings.  The strings represent size-variable names, which
+          will be in-scope for the C-code.  Size-variables have type 'int'
+
+        NOTE: If `shape` is a length-0 tuple, then `new_name` should have
+              C-type `prim_type` and be stack-allocated if appropriate,
+              whereas if `shape` is not a length-0 tuple,
+              then `new_name` should have C-type `prim_type *`.
+
+        NOTE Further: if a given memory can only support pointers to it,
+                      then attempting to allocate a variable with shape ()
+                      should trigger an error rather than do something weird.
+                      The user is then responsible for converting the code
+                      to use a buffer of type prim_type[1], which is
+                      equivalent to a scalar, but will be processed as
+                      a pointer-indirected buffer at the C-level.
+
+        Memory Ordering: The memory may choose an ordering of the buffer, but
+                         should prefer a "row-major" layout in which the last
+                         dimension of shape is iterated most frequently
+                         and the first dimension of shape is iterated least
+                         frequently.
+
+        Error Handling:
+            If the memory is unable to successfully generate an
+            allocation, free, read, write, or reduction, then the
+            macro invoked should raise a MemGenError with a useful
+            message.  This error will be reported to the user.
+    """
+    if prim_type is "float":
+        return ("float {new_name};")
+    else:
+        size_str = shape[0]
+        for s in shape[1:]:
+            size_str = f"{s} * {size_str}"
+        return (f"{prim_type} *{new_name} = " +
+                f"({prim_type}}*) malloc ({size_str} * sizeof({prim_type}));")
+
+
 def dram_free(x):
     if type(x) is TL_float:
         return ("")
