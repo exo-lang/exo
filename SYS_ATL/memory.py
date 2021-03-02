988bcf758cafe21ede9e4457a199343c4df2574f
Yuka Takahashi <yukatkh@gmail.com>
2021-03-02 13:53:33 -0500

    update memory management sketch

--- SYS_ATL/memory.py
+++ SYS_ATL/memory.py
@@ -22,8 +22,25 @@ from . import shared_types as T
 #           * reduce to the memory (optional)
 """
 
+# This MetaData will be compiled into
+# struct GEMM_req_metadata {
+class MetaData:
+    def __init__(self,
+        row_size,
+        col_size,
+        buf_type,
+        buf_bits,
+    ):
+    
+    self.row_size = row_size
+    self.col_size = col_size
+    self.buf_type = buf_type
+    self.buf_bits = buf_bits
+
+
 class Memory:
     def __init__(self, name,
+        metadata= None,
         alloc   = None,
         free    = None,
         read    = None,
@@ -32,6 +49,7 @@ class Memory:
     ):
 
     self._name  = name
+    self._meta  = metadata
     self._alloc = alloc
     self._free  = free
     self._read  = read
@@ -41,15 +59,32 @@ class Memory:
 # TODO: How to encode size??
 # Or assume that alloc/free code must not change by types?
 # How to handle different types? e.g. float, double,..
-if s.type is T.R:
-    self.add_line(f"float {name};")
-else:
-    size = _type_size(s.type, self.env)
-    self.add_line(f"float *{name} = " +
-            f"(float*) malloc ({size} * sizeof(float));")
+#
+#if s.type is T.R:
+#    self.add_line(f"float {name};")
+#else:
+#    size = _type_size(s.type, self.env)
+#    self.add_line(f"float *{name} = " +
+#            f"(float*) malloc ({size} * sizeof(float));")
+# Check this in typecheck.py, isinstance(Memory)
+# We should at least create TL_float
 
+# Write some compiler-ish code here?
+def dram_alloc(x) :
+    if type(x) is TL_float:
+        return ("float {x};")
+    elif type(x) is TL_double:
+        return ("double {x};")
+def dram_free(x):
+    if type(x) is TL_float:
+        return ("")
+    else:
+        return ("free {x};")
 DRAM = Memory("DRAM",
-        alloc = 
+        alloc = dram_alloc,
+        free = dram_free,
+        ...
+    )
 
 
 """
@@ -80,6 +115,3 @@ void GEMM_free( void *buf, req_metadata req ) {
 }
 
 """
-
-
-A memory is
