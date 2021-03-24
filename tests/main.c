ae0a53b5d9122b08f073e7685b32658e85858d83
Yuka Takahashi <yukatkh@gmail.com>
2021-03-24 15:46:19 -0400

    simple example works on gemmini

--- tests/main.c
+++ tests/main.c
@@ -1,3 +1,5 @@
+// riscv64-unknown-elf-gcc  -DPREALLOCATE=1 -DMULTITHREAD=1 -mcmodel=medany -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -march=rv64gc -Wa,-march=rv64gcxhwacha -lm -lgcc -I/home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests -I/home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests/env -I/home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests -I/home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests/benchmarks/common -DID_STRING=  -nostdlib -nostartfiles -static -T /home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests/benchmarks/common/test.ld -DBAREMETAL=1  /home/yuka/SYS_ATL/tests/main.c /home/yuka/SYS_ATL/tests/tmp/test_load_16.c -o main /home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests/benchmarks/common/crt.S /home/yuka/chipyard/generators/gemmini/software/gemmini-rocc-tests/riscv-tests/benchmarks/common/syscalls.c
+//
 #include <stdint.h>
 #include <stddef.h>
 #include <assert.h>
@@ -5,10 +7,10 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "include/gemmini_testutils.h"
 #include "tmp/test_load_16.h"
 
 int main() {
-    pin_all();
     gemmini_flush(0);
 
     float x[16][16];
