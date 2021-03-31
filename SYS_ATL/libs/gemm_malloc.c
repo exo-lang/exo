65e3eb4546d7d2fca828a1771025b2d40e644765
Yuka Takahashi <yukatkh@gmail.com>
2021-03-31 15:42:46 -0400

    working on gemm_malloc

--- SYS_ATL/libs/gemm_malloc.c
+++ SYS_ATL/libs/gemm_malloc.c
@@ -1,14 +1,28 @@
-#define HEAP_SIZE {heap_size}
+//#define HEAP_SIZE {heap_size}
+//#define DIM {dim}
+#define HEAP_SIZE 1000
+#define DIM 16
 
+#include<stdio.h>
 #include<stdint.h>
+#include<assert.h>
 
+uint32_t _ceil(float num) {
+    int inum = (int)num;
+    if (num == (float)inum) {
+        return inum;
+    }
+    return inum + 1;
+}
+
+// Get enough HEAP for FreeBlock
 uint8_t HEAP[HEAP_SIZE];
 
 // https://stackoverflow.com/questions/5473189/what-is-a-packed-structure-in-c
 typedef struct __attribute__((__packed__)) FreeBlock {
     uint32_t size;
     struct FreeBlock *next;
-    uint8_t data[0];
+    uint32_t loc;
 } FreeBlock;
 FreeBlock *freelist;
 
@@ -20,28 +34,27 @@ void init_mem() {
 
     p++;
     p->next = 0;
-    p->size = HEAP_SIZE - sizeof(FreeBlock);
+    p->size = (HEAP_SIZE/DIM);
 }
 
 void *search(FreeBlock *cur, uint32_t bytes) {
     FreeBlock *prev = freelist;
 
     for(;;) {
-        uint32_t size = sizeof(uint32_t) + sizeof(FreeBlock*);
-
-        if (cur->next == 0 && cur->size < bytes + size) {
+        if (cur->next == 0 && cur->size < bytes) {
             fprintf(stderr, "Out of memory!\n");
             return 0;
-        } else if (cur->next == 0 && cur->size >= (bytes + size)) {
+        } else if (cur->next == 0 && cur->size >= bytes) {
             // cut cur into bytes blocks and create new
             uint32_t sz = cur->size;
-            FreeBlock *new = (void *)cur + bytes + size;
+            FreeBlock *new = (void *)cur + sizeof(FreeBlock);
             new->next = 0;
-            new->size = sz - bytes - size;
-            cur->size = bytes + size;
+            new->size = sz - bytes;
+            new->loc  = cur->loc + bytes;
+            cur->size = bytes;
             prev->next = new;
             break;
-        } else if (cur->size >= (bytes + size)) {
+        } else if (cur->size >= bytes) {
             prev->next = cur->next;
             break;
         } else {
@@ -50,26 +63,57 @@ void *search(FreeBlock *cur, uint32_t bytes) {
         }
     }
 
-    return cur->data;
+    return cur->loc;
 }
 
-void *malloc_dram(long unsigned int bytes) {
-    bytes = bytes < sizeof(FreeBlock) ? sizeof(FreeBlock) : bytes;
-    if (bytes == 0) return 0;
-    FreeBlock *loc = search(freelist, bytes);
-    if (loc == 0)
-        return 0;
-    else {
-        return loc;
+uint32_t malloc_gemm(long unsigned int bytes) {
+    assert(bytes != 0);
+    uint32_t b = _ceil(((float)bytes)/(float)DIM);
+    uint32_t loc = search(freelist, b);
+    
+    return loc;
+}
+
+void *search_free(uint32_t ptr) {
+    FreeBlock *cur = freelist;
+
+    for(;;) {
+        if (cur->next == 0 && cur->loc != ptr) {
+            fprintf(stderr, "No such ptr!\n");
+            return 0;
+        } else if (cur->loc == ptr) {
+            return cur;
+        } else {
+            cur = cur->next;
+        }
     }
+    return 0;
 }
 
-void free_dram(void *ptr) {
-    if (ptr == 0) return;
-    ptr = ptr -  sizeof(uint32_t) - sizeof(FreeBlock*);
+void free_gemm(uint32_t ptr) {
+    FreeBlock* p = search_free(ptr);
     FreeBlock *next = freelist->next;
-    freelist->next = ptr;
-    ((FreeBlock*)ptr)->next = next;
+    freelist->next = p;
+    ((FreeBlock*)p)->next = next;
 
     return;
+}
+
+int main(void) {
+  fprintf(stderr, "calling init_mem\n");
+  init_mem();
+  uint32_t zero = malloc_gemm(10);
+  fprintf(stderr, "zero: %d\n", zero);
+  uint32_t one = malloc_gemm(20);
+  fprintf(stderr, "one: %d\n", one);
+  uint32_t three = malloc_gemm(40);
+  fprintf(stderr, "three: %d\n", three);
+  uint32_t six = malloc_gemm(100);
+  fprintf(stderr, "six: %d\n", six);
+
+  free_gemm(one);
+  uint32_t one2 = malloc_gemm(20);
+  fprintf(stderr, "one2: %d\n", one2);
+
+  return 0;
 }
