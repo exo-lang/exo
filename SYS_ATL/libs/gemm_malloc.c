080f4fa9407d776358123f67119701933153412a
Yuka Takahashi <yukatkh@gmail.com>
2021-04-01 09:12:32 -0400

    wip gemm_malloc

--- SYS_ATL/libs/gemm_malloc.c
+++ SYS_ATL/libs/gemm_malloc.c
@@ -15,19 +15,28 @@ uint32_t _ceil(float num) {
     return inum + 1;
 }
 
-// Get enough HEAP for FreeBlock
-uint8_t HEAP[HEAP_SIZE];
+// Get enough HEAP for Block
+uint8_t HEAP[HEAP_SIZE/16];
+uint8_t USED[HEAP_SIZE/16];
 
 // https://stackoverflow.com/questions/5473189/what-is-a-packed-structure-in-c
-typedef struct __attribute__((__packed__)) FreeBlock {
+typedef struct __attribute__((__packed__)) Block {
     uint32_t size;
-    struct FreeBlock *next;
+    struct Block *next;
     uint32_t loc;
-} FreeBlock;
-FreeBlock *freelist;
+} Block;
+Block *freelist;
+
+typedef struct __attribute__((__packed__)) UsedBlock {
+    uint32_t size;
+    struct UsedBlock *next;
+    struct Block     *freeblock;
+    uint32_t loc;
+} UsedBlock;
+UsedBlock *usedlist;
 
 void init_mem() {
-    FreeBlock *p = (FreeBlock *)HEAP;
+    Block *p = (Block *)HEAP;
     p->next = p+1;
     p->size = 0;
     freelist = p;
@@ -35,11 +44,18 @@ void init_mem() {
     p++;
     p->next = 0;
     p->size = (HEAP_SIZE/DIM);
+
+    UsedBlock *b = (UsedBlock *)USED;
+    b->next  = b+1;
+    b->size  = 0;
+    usedlist = b;
+    b++;
+    b->next  = 0;
 }
 
-void *search(FreeBlock *cur, uint32_t bytes) {
-    FreeBlock *prev = freelist;
-
+void *search(uint32_t bytes) {
+    Block *cur = freelist;
+    Block *prev = freelist;
     for(;;) {
         if (cur->next == 0 && cur->size < bytes) {
             fprintf(stderr, "Out of memory!\n");
@@ -47,7 +63,7 @@ void *search(FreeBlock *cur, uint32_t bytes) {
         } else if (cur->next == 0 && cur->size >= bytes) {
             // cut cur into bytes blocks and create new
             uint32_t sz = cur->size;
-            FreeBlock *new = (void *)cur + sizeof(FreeBlock);
+            Block *new = (void *)cur + sizeof(Block);
             new->next = 0;
             new->size = sz - bytes;
             new->loc  = cur->loc + bytes;
@@ -63,21 +79,36 @@ void *search(FreeBlock *cur, uint32_t bytes) {
         }
     }
 
+    UsedBlock *prev_used = usedlist;
+    UsedBlock *cur_used  = usedlist;
+    for(;;) {
+        if (cur_used->next == 0) {
+            UsedBlock* new = (void *)cur_used + sizeof(UsedBlock);
+            new->next = 0;
+            new->size = cur->size;
+            new->loc = cur->loc;
+            new->freeblock = cur;
+            prev_used->next = new;
+            break;
+        } else {
+            prev_used = cur_used;
+            cur_used  = cur_used->next;
+        }
+    }
+
     return cur->loc;
 }
 
 uint32_t malloc_gemm(long unsigned int bytes) {
     assert(bytes != 0);
     uint32_t b = _ceil(((float)bytes)/(float)DIM);
-    uint32_t loc = search(freelist, b);
+    uint32_t loc = search(b);
     
     return loc;
 }
 
-// TODO: How to free?? We don't have address.
-// Keep track of "not-free-list"??
 void *search_free(uint32_t ptr) {
-    FreeBlock *cur = freelist;
+    UsedBlock *cur = usedlist;
 
     for(;;) {
         printf("cur->loc: %d\n", cur->loc);
@@ -85,7 +116,7 @@ void *search_free(uint32_t ptr) {
             fprintf(stderr, "No such ptr!\n");
             return 0;
         } else if (cur->loc == ptr) {
-            return cur;
+            return cur->freeblock;
         } else {
             cur = cur->next;
         }
@@ -94,10 +125,10 @@ void *search_free(uint32_t ptr) {
 }
 
 void free_gemm(uint32_t ptr) {
-    FreeBlock* p = search_free(ptr);
-    FreeBlock *next = freelist->next;
+    Block* p = search_free(ptr);
+    Block *next = freelist->next;
     freelist->next = p;
-    ((FreeBlock*)p)->next = next;
+    ((Block*)p)->next = next;
 
     return;
 }
