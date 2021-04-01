cda1db15b047caff2fe076fd9bbedf6b451ec38b
Yuka Ikarashi <yukatkh@gmail.com>
2021-04-01 12:02:04 -0400

    gemm_malloc written in array working

--- SYS_ATL/libs/gemm_malloc.c
+++ SYS_ATL/libs/gemm_malloc.c
@@ -7,132 +7,54 @@
 #include<stdint.h>
 #include<assert.h>
 
-uint32_t _ceil(float num) {
-    int inum = (int)num;
-    if (num == (float)inum) {
-        return inum;
-    }
-    return inum + 1;
-}
-
-// Get enough HEAP for Block
-uint8_t HEAP[HEAP_SIZE/16];
-uint8_t USED[HEAP_SIZE/16];
-
-// https://stackoverflow.com/questions/5473189/what-is-a-packed-structure-in-c
-typedef struct __attribute__((__packed__)) Block {
+typedef struct __attribute__((__packed__)) NewBlock {
     uint32_t size;
-    struct Block *next;
     uint32_t loc;
-} Block;
-Block *freelist;
-
-typedef struct __attribute__((__packed__)) UsedBlock {
-    uint32_t size;
-    struct UsedBlock *next;
-    struct Block     *freeblock;
-    uint32_t loc;
-} UsedBlock;
-UsedBlock *usedlist;
+    uint8_t is_used;
+} NewBlock;
+NewBlock BLOCKS[HEAP_SIZE / sizeof(NewBlock)];
+uint32_t gemm_last_ptr;
 
 void init_mem() {
-    Block *p = (Block *)HEAP;
-    p->next = p+1;
-    p->size = 0;
-    freelist = p;
-
-    p++;
-    p->next = 0;
-    p->size = (HEAP_SIZE/DIM);
-
-    UsedBlock *b = (UsedBlock *)USED;
-    b->next  = b+1;
-    b->size  = 0;
-    usedlist = b;
-    b++;
-    b->next  = 0;
+  for(uint32_t i=0; i<sizeof(BLOCKS); i++)
+    ((uint8_t*)BLOCKS)[i] = 0;
+  gemm_last_ptr = 0;
 }
 
-void *search(uint32_t bytes) {
-    Block *cur = freelist;
-    Block *prev = freelist;
-    for(;;) {
-        if (cur->next == 0 && cur->size < bytes) {
-            fprintf(stderr, "Out of memory!\n");
-            return 0;
-        } else if (cur->next == 0 && cur->size >= bytes) {
-            // cut cur into bytes blocks and create new
-            uint32_t sz = cur->size;
-            Block *new = (void *)cur + sizeof(Block);
-            new->next = 0;
-            new->size = sz - bytes;
-            new->loc  = cur->loc + bytes;
-            cur->size = bytes;
-            prev->next = new;
-            break;
-        } else if (cur->size >= bytes) {
-            prev->next = cur->next;
-            break;
-        } else {
-            prev = cur;
-            cur = cur->next;
-        }
+uint32_t malloc_gemm(long unsigned int size) {
+  if(size == 0) return -1;
+  size = (size + DIM - 1) / DIM;
+  int i;
+  for(i=0; i < HEAP_SIZE / sizeof(NewBlock) && BLOCKS[i].size > 0; i++) {
+    if(BLOCKS[i].is_used) continue;
+    if(BLOCKS[i].size < size) continue;
+    break;
+  }
+  if(BLOCKS[i].size == 0) {
+    // initialize current_block_index
+    BLOCKS[i].loc = gemm_last_ptr;
+    BLOCKS[i].size = size;
+    BLOCKS[i].is_used = 1;
+    gemm_last_ptr += size;
+    return BLOCKS[i].loc;
+  }
+
+  BLOCKS[i].is_used = 1;
+  return BLOCKS[i].loc;
+}
+
+void free_gemm(uint32_t addr) {
+  for(int i=0; BLOCKS[i].size > 0; i++) {
+    if(BLOCKS[i].is_used && BLOCKS[i].loc == addr) {
+      BLOCKS[i].is_used = 0;
+      return;
     }
-
-    UsedBlock *prev_used = usedlist;
-    UsedBlock *cur_used  = usedlist;
-    for(;;) {
-        if (cur_used->next == 0) {
-            UsedBlock* new = (void *)cur_used + sizeof(UsedBlock);
-            new->next = 0;
-            new->size = cur->size;
-            new->loc = cur->loc;
-            new->freeblock = cur;
-            prev_used->next = new;
-            break;
-        } else {
-            prev_used = cur_used;
-            cur_used  = cur_used->next;
-        }
-    }
-
-    return cur->loc;
+  }
+  return;
 }
 
-uint32_t malloc_gemm(long unsigned int bytes) {
-    assert(bytes != 0);
-    uint32_t b = _ceil(((float)bytes)/(float)DIM);
-    uint32_t loc = search(b);
-    
-    return loc;
-}
-
-void *search_free(uint32_t ptr) {
-    UsedBlock *cur = usedlist;
-
-    for(;;) {
-        printf("cur->loc: %d\n", cur->loc);
-        if (cur->next == 0 && cur->loc != ptr) {
-            fprintf(stderr, "No such ptr!\n");
-            return 0;
-        } else if (cur->loc == ptr) {
-            return cur->freeblock;
-        } else {
-            cur = cur->next;
-        }
-    }
-    return 0;
-}
-
-void free_gemm(uint32_t ptr) {
-    Block* p = search_free(ptr);
-    Block *next = freelist->next;
-    freelist->next = p;
-    ((Block*)p)->next = next;
-
-    return;
-}
-
+#include <stdio.h>
+#include <stdlib.h>
 int main(void) {
   fprintf(stderr, "calling init_mem\n");
   init_mem();
@@ -150,6 +72,18 @@ int main(void) {
   free_gemm(one);
   uint32_t one2 = malloc_gemm(20);
   fprintf(stderr, "one2: %d\n", one2);
+  free_gemm(_13);
+  uint32_t _13_2 = malloc_gemm(300);
+  fprintf(stderr, "_13_2: %d\n", _13_2);
+
+  uint32_t t = malloc_gemm(30);
+  fprintf(stderr, "t: %d\n", t);
+
+  for (int i = 0; i < 100; i ++) {
+    uint32_t ptr = malloc_gemm(rand()%100);
+    printf("%d\n", ptr);
+    free_gemm(ptr);
+  }
 
   return 0;
 }
