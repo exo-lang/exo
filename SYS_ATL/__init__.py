d4cc2f3c095ee1ff97313ffe05faa1ab7e1e92fe
Alex Reinking <alex.reinking@gmail.com>
2021-10-12 17:01:37 -0700

    Optimize imports, fix __all__ in __init__.py

--- SYS_ATL/__init__.py
+++ SYS_ATL/__init__.py
@@ -1,16 +1,16 @@
-# install pretty-printing
 from . import LoopIR_pprint
 from .API import Procedure, compile_procs, proc, instr, config
 from .configs import Config
 from .memory import Memory, DRAM
 
 __all__ = [
+    "LoopIR_pprint",
+    "Procedure",
+    "compile_procs",
     "proc",
     "instr",
-    "Procedure",
     "config",
     "Config",
-    "DRAM",
     "Memory",
-    'compile_procs',
+    "DRAM",
 ]
