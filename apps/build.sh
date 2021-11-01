de783adcbfaf9e9b9d739f19539420d7eab335f3
Alex Reinking <alex.reinking@gmail.com>
2021-11-01 14:04:04 -0700

    Use Google benchmark for benchmarking SGEMM

---
+++ apps/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Meant to be run from the root of the repo:
+# ./apps/build.sh
+
+cmake -G Ninja -S dependencies/benchmark -B build/benchmark \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBENCHMARK_ENABLE_TESTING=NO \
+  -DCMAKE_INSTALL_PREFIX="$PWD/build/_install"
+
+cmake --build build/benchmark --target install
+
+cmake -G Ninja -S apps -B build/apps \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH="$PWD/build/_install" \
+  -DCMAKE_C_FLAGS="-march=skylake-avx512" \
+  -DCMAKE_CXX_FLAGS="-march=skylake-avx512"
+
+cmake --build build/apps
