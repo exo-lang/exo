5d403499a0ab3a22c728f9ebae36b7ee815503ad
Alex Reinking <alex.reinking@gmail.com>
2021-11-02 16:14:28 -0700

    Reduce code duplication

--- apps/x86_demo/run_sgemm.cpp
+++ apps/x86_demo/run_sgemm.cpp
@@ -8,6 +8,9 @@
 
 #include "alex_sgemm.h"
 
+// ----------------------------------------------------------------------------
+// Utilities
+
 double num_flops(long m, long n, long k) { return 2 * m * n * k; }
 
 std::vector<float> gen_matrix(long m, long n) {
@@ -20,6 +23,9 @@ std::vector<float> gen_matrix(long m, long n) {
 
   return mat;
 }
+
+// ----------------------------------------------------------------------------
+// Benchmarking just the inner kernel
 
 static void BM_sys_atl_kernel(benchmark::State &state) {
   size_t k = state.range(0);
@@ -39,49 +45,19 @@ static void BM_sys_atl_kernel(benchmark::State &state) {
   );
 }
 
-BENCHMARK(BM_sys_atl_kernel)->Range(8, 8196);
+BENCHMARK(BM_sys_atl_kernel)->Name("kernel_sys_atl")->Range(8, 8196);
 
-static void BM_mkl_kernel(benchmark::State &state) {
-  size_t k = state.range(0);
-  auto a = gen_matrix(6, k);
-  auto b = gen_matrix(k, 64);
-  auto c = gen_matrix(6, 64);
+// ----------------------------------------------------------------------------
+// Benchmarking various SGEMM implementations on square sizes.
 
-  for (auto _ : state) {
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, // layout
-                6, 64, k,                                  // m, n, k
-                1.0,                                       // alpha
-                a.data(), k,                               // A (lda)
-                b.data(), 64,                              // B (ldb)
-                1.0,                                       // beta
-                c.data(), 64                               // C (ldc)
-    );
-  }
-
-  state.counters["flops"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_flops(6, 64, k)), //
-      benchmark::Counter::kIsRate,                                   //
-      benchmark::Counter::kIs1000                                    //
-  );
-}
-
-BENCHMARK(BM_mkl_kernel)->Range(8, 8196);
-
-static void BM_mkl_sgemm(benchmark::State &state) {
+template <typename SgemmFn> void BM_square_sgemm(benchmark::State &state) {
   size_t n = state.range(0);
   auto a = gen_matrix(n, n);
   auto b = gen_matrix(n, n);
   auto c = gen_matrix(n, n);
 
   for (auto _ : state) {
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, // layout
-                n, n, n,                                   // m, n, k
-                1.0,                                       // alpha
-                a.data(), n,                               // A (lda)
-                b.data(), n,                               // B (ldb)
-                1.0,                                       // beta
-                c.data(), n                                // C (ldc)
-    );
+    SgemmFn{}(a.data(), b.data(), c.data(), n);
   }
 
   state.counters["flops"] = benchmark::Counter(
@@ -91,7 +67,27 @@ static void BM_mkl_sgemm(benchmark::State &state) {
   );
 }
 
-BENCHMARK(BM_mkl_sgemm)
+struct mkl_square {
+  void operator()(const float *a, const float *b, float *c, long n) {
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, // layout
+                n, n, n,                                   // m, n, k
+                1.0,                                       // alpha
+                a, n,                                      // A (lda)
+                b, n,                                      // B (ldb)
+                1.0,                                       // beta
+                c, n                                       // C (ldc)
+    );
+  }
+};
+
+struct alex_square {
+  void operator()(const float *a, const float *b, float *c, long n) {
+    sgemm_square(a, b, c, n);
+  }
+};
+
+BENCHMARK_TEMPLATE(BM_square_sgemm, mkl_square)
+    ->Name("sgemm_mkl")
     ->DenseRange(64, 1984, 128)
     ->Arg(221)
     ->Arg(256)
@@ -103,24 +99,8 @@ BENCHMARK(BM_mkl_sgemm)
     ->Arg(1024)
     ->Arg(2048);
 
-static void BM_alex_sgemm(benchmark::State &state) {
-  size_t n = state.range(0);
-  auto a = gen_matrix(n, n);
-  auto b = gen_matrix(n, n);
-  auto c = gen_matrix(n, n);
-
-  for (auto _ : state) {
-    sgemm_square(a.data(), b.data(), c.data(), n);
-  }
-
-  state.counters["flops"] = benchmark::Counter(
-      static_cast<double>(state.iterations() * num_flops(n, n, n)), //
-      benchmark::Counter::kIsRate,                                  //
-      benchmark::Counter::kIs1000                                   //
-  );
-}
-
-BENCHMARK(BM_alex_sgemm)
+BENCHMARK_TEMPLATE(BM_square_sgemm, alex_square)
+    ->Name("sgemm_alex")
     ->DenseRange(64, 1984, 128)
     ->Arg(221)
     ->Arg(256)
