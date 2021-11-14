20f4903a02e9ad84623e735526e328cc2c7361bd
Alex Reinking <alex_reinking@berkeley.edu>
2021-11-14 15:53:08 -0500

    Try adding masks to sgemm

--- apps/x86_demo/sgemm/sgemm.py
+++ apps/x86_demo/sgemm/sgemm.py
@@ -1,10 +1,59 @@
 from __future__ import annotations
 
-from SYS_ATL.LoopIR_compiler import run_compile
-
 from SYS_ATL import *
 from SYS_ATL.platforms.x86 import *
 from SYS_ATL.syntax import *
+
+
+def new_config_x86():
+    @config
+    class ConfigLoad:
+        masked_N: size
+
+    return ConfigLoad
+
+
+Config = new_config_x86()
+
+
+# TODO: We need a better way of stack-allocating masks / config
+@instr('__mmask16 dumb_mask = (1 << {N}) - 1;')
+def mk_mask(N: size):
+    Config.masked_N = N
+
+
+@instr('{dst_data} = _mm512_maskz_loadu_ps(dumb_mask, &{src_data});')
+def mm512_maskz_dumb_loadu_ps(
+        masked_N : size,
+        dst: [f32][16] @ AVX512,
+        src: [f32][masked_N] @ DRAM,
+):
+    assert stride(src, 0) == 1
+    assert stride(dst, 0) == 1
+    assert masked_N > 0
+    assert masked_N <= 16
+
+    for i in par(0, 16):
+        if i < masked_N:
+            dst[i] = src[i]
+        else:
+            dst[i] = 0.0
+
+
+@instr('_mm512_mask_storeu_ps(&{dst_data}, dumb_mask, {src_data});')
+def mm512_mask_dumb_storeu_ps(
+        masked_N: size,
+        dst: [f32][masked_N] @ DRAM,
+        src: [f32][16] @ AVX512
+):
+    assert stride(src, 0) == 1
+    assert stride(dst, 0) == 1
+    assert masked_N > 0
+    assert masked_N <= 16
+
+    for i in par(0, 16):
+        if i < masked_N:
+            dst[i] = src[i]
 
 
 # noinspection PyPep8Naming
@@ -24,13 +73,15 @@ def sgemm_masked_kernel_avx512_template(
     assert stride(B, 1) == 1
     assert stride(C, 1) == 1
 
+    mk_mask(N % 16)
+
     C_reg: f32[M, ((N + 15) / 16), 16] @ AVX512
     for i in par(0, M):
         for j in par(0, N / 16):
             mm512_loadu_ps(C_reg[i, j, :], C[i, 16 * j:16 * j + 16])
         if N % 16 > 0:
-            mm512_maskz_loadu_ps(
-                N % 16,
+            mm512_maskz_dumb_loadu_ps(
+                Config.masked_N,
                 C_reg[i, N / 16, :],
                 C[i, 16 * (N / 16):16 * (N / 16) + N % 16]
             )
@@ -48,8 +99,8 @@ def sgemm_masked_kernel_avx512_template(
         for j in par(0, N / 16):
             mm512_storeu_ps(C[i, 16 * j:16 * j + 16], C_reg[i, j, :])
         if N % 16 > 0:
-            mm512_mask_storeu_ps(
-                N % 16,
+            mm512_mask_dumb_storeu_ps(
+                Config.masked_N,
                 C[i, 16 * (N / 16):16 * (N / 16) + N % 16],
                 C_reg[i, N / 16, :]
             )
