2cecbfb0a72bd74402c50926e6dd1128ee2bf36d
Alex Reinking <alex.reinking@gmail.com>
2022-01-18 18:00:46 -0800

    Merge configs.py and conversions.py

--- SYS_ATL/asts/configs.py
+++ SYS_ATL/asts/configs.py
@@ -1,5 +1,20 @@
-from .conversions import uast_to_type
 from ..prelude import Sym
+
+
+def uast_to_type(typ):
+    from . import LoopIR
+    from . import UAST
+
+    return {
+        UAST.Size():   LoopIR.Size(),
+        UAST.Bool():   LoopIR.Bool(),
+        UAST.Index():  LoopIR.Index(),
+        UAST.Stride(): LoopIR.Stride(),
+        UAST.F32():    LoopIR.F32(),
+        UAST.F64():    LoopIR.F64(),
+        UAST.INT8():   LoopIR.INT8(),
+        UAST.INT32():  LoopIR.INT32(),
+    }[typ]
 
 
 ################################################################################
