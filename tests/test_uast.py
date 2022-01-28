498d1354f631b485bbc1a55cdda2896e350f32bf
Alex Reinking <alex.reinking@gmail.com>
2022-01-27 22:03:52 -0800

    Format test_uast.py

--- tests/test_uast.py
+++ tests/test_uast.py
@@ -7,16 +7,17 @@ from SYS_ATL import instr, Procedure, DRAM
 
 def gen_conv1d():
     @instr("TEST", _testing="UAST")
-    def conv1d(n : size, m : size, r: size,
-               x : R[n], w : R[m], res : R[r] ):
-        for i in par(0,r):
+    def conv1d(n: size, m: size, r: size,
+               x: R[n], w: R[m], res: R[r]):
+        for i in par(0, r):
             res[i] = 0.0
-        for i in par(0,r):
-            for j in par(0,n):
+        for i in par(0, r):
+            for j in par(0, n):
                 if i <= j < i + m:
-                    res[i] += x[j]*w[i-j+m-1]
+                    res[i] += x[j] * w[i - j + m - 1]
 
     return conv1d
+
 
 def test_conv1d():
     conv1d = gen_conv1d()
@@ -38,22 +39,23 @@ def test_unary_neg():
 
 def gen_alloc_nest():
     @instr("TEST", _testing="UAST")
-    def alloc_nest(n : size, m : size,
-                   x : R[n,m], y: R[n,m] @ DRAM, res : R[n,m] @ DRAM):
-        for i in par(0,n):
-            rloc : R[m] @DRAM
-            xloc : R[m] @DRAM
-            yloc : R[m] @DRAM
-            for j in par(0,m):
-                xloc[j] = x[i,j]
-            for j in par(0,m):
-                yloc[j] = y[i,j]
-            for j in par(0,m):
+    def alloc_nest(n: size, m: size,
+                   x: R[n, m], y: R[n, m] @ DRAM, res: R[n, m] @ DRAM):
+        for i in par(0, n):
+            rloc: R[m] @ DRAM
+            xloc: R[m] @ DRAM
+            yloc: R[m] @ DRAM
+            for j in par(0, m):
+                xloc[j] = x[i, j]
+            for j in par(0, m):
+                yloc[j] = y[i, j]
+            for j in par(0, m):
                 rloc[j] = xloc[j] + yloc[j]
-            for j in par(0,m):
-                res[i,j] = rloc[j]
+            for j in par(0, m):
+                res[i, j] = rloc[j]
 
     return alloc_nest
+
 
 def test_alloc_nest():
     alloc = gen_alloc_nest()
