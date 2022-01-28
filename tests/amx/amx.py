4627e01e6654e414bbc8bd7eceb546e70302acc8
Alex Reinking <alex.reinking@gmail.com>
2022-01-28 00:09:05 -0800

    Make AMX use primary compiler fixture.

--- tests/amx/amx.py
+++ tests/amx/amx.py
@@ -3,47 +3,48 @@ from __future__ import annotations
 from SYS_ATL import instr, DRAM
 from SYS_ATL.libs.memories import AMX_TILE
 
-# --------------------------------------------------------------------------- #
-#   Instructions
-# --------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# Config                                                                       #
+# ---------------------------------------------------------------------------- #
 
 _amx_config = ("""
 unsigned char config[] = {{
-    0x01, // ID
-    0x00, // start row
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // reserved
-    64, 0x00, // bytes per row tile 0
-    64, 0x00, // bytes per row tile 1
-    64, 0x00, // bytes per row tile 2
-    0x00, 0x00, // bytes per row tile 3
-    0x00, 0x00, // bytes per row tile 4
-    0x00, 0x00, // bytes per row tile 5
-    0x00, 0x00, // bytes per row tile 6
-    0x00, 0x00, // bytes per row tile 7
-    0x00, 0x00, // bytes per row tile 8
-    0x00, 0x00, // bytes per row tile 9
-    0x00, 0x00, // bytes per row tile 10
-    0x00, 0x00, // bytes per row tile 11
-    0x00, 0x00, // bytes per row tile 12
-    0x00, 0x00, // bytes per row tile 13
-    0x00, 0x00, // bytes per row tile 14
-    0x00, 0x00, // bytes per row tile 15
-    16, // rows tile 0
-    16, // rows tile 1
-    16, // rows tile 2
-    0x00, // rows tile 3
-    0x00, // rows tile 4
-    0x00, // rows tile 5
-    0x00, // rows tile 6
-    0x00, // rows tile 7
-    0x00, // rows tile 8
-    0x00, // rows tile 9
-    0x00, // rows tile 10
-    0x00, // rows tile 11
-    0x00, // rows tile 12
-    0x00, // rows tile 13
-    0x00, // rows tile 14
-    0x00 // rows tile 15
+    0x01,                                     // ID
+    0x00,                                     // start row
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // reserved
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // reserved
+    64, 0x00,                                 // bytes per row tile 0
+    64, 0x00,                                 // bytes per row tile 1
+    64, 0x00,                                 // bytes per row tile 2
+    0x00, 0x00,                               // bytes per row tile 3
+    0x00, 0x00,                               // bytes per row tile 4
+    0x00, 0x00,                               // bytes per row tile 5
+    0x00, 0x00,                               // bytes per row tile 6
+    0x00, 0x00,                               // bytes per row tile 7
+    0x00, 0x00,                               // bytes per row tile 8
+    0x00, 0x00,                               // bytes per row tile 9
+    0x00, 0x00,                               // bytes per row tile 10
+    0x00, 0x00,                               // bytes per row tile 11
+    0x00, 0x00,                               // bytes per row tile 12
+    0x00, 0x00,                               // bytes per row tile 13
+    0x00, 0x00,                               // bytes per row tile 14
+    0x00, 0x00,                               // bytes per row tile 15
+    16,                                       // rows tile 0
+    16,                                       // rows tile 1
+    16,                                       // rows tile 2
+    0x00,                                     // rows tile 3
+    0x00,                                     // rows tile 4
+    0x00,                                     // rows tile 5
+    0x00,                                     // rows tile 6
+    0x00,                                     // rows tile 7
+    0x00,                                     // rows tile 8
+    0x00,                                     // rows tile 9
+    0x00,                                     // rows tile 10
+    0x00,                                     // rows tile 11
+    0x00,                                     // rows tile 12
+    0x00,                                     // rows tile 13
+    0x00,                                     // rows tile 14
+    0x00                                      // rows tile 15
 }};
 _tile_loadconfig(config);
 """)
@@ -54,8 +55,13 @@ def config():
     pass
 
 
+# ---------------------------------------------------------------------------- #
+# ld_i8, ld_i8_3d                                                              #
+# ---------------------------------------------------------------------------- #
+
+
 # TODO: Handle custom read_stride
-_amx_ld_i8 = ("_tile_loadd({dst_int}, {src}.data, {src}.strides[0]);")
+_amx_ld_i8 = "_tile_loadd({dst_int}, {src}.data, {src}.strides[0]);"
 
 
 @instr(_amx_ld_i8)
@@ -93,11 +99,12 @@ def ld_i8_3d(
                 dst[i, j, k] = src[i, 4 * j + k]
 
 
+# ---------------------------------------------------------------------------- #
+# st_i8, st_i32, zero_i32                                                      #
+# ---------------------------------------------------------------------------- #
+
 # TODO: Handle custom write_stride
-_amx_st_i8 = ("_tile_stored({src_int}, {dst}.data, {dst}.strides[0]);")
-
-
-@instr(_amx_st_i8)
+@instr('_tile_stored({src_int}, {dst}.data, {dst}.strides[0]);')
 def st_i8(
         m: size,
         n: size,
@@ -111,10 +118,7 @@ def st_i8(
             dst[i, j] = src[i, j]
 
 
-_amx_st_i32 = ("_tile_stored({src_int}, {dst}.data, 4*{dst}.strides[0]);")
-
-
-@instr(_amx_st_i32)
+@instr('_tile_stored({src_int}, {dst}.data, 4*{dst}.strides[0]);')
 def st_i32(
         m: size,
         n: size,
@@ -128,10 +132,7 @@ def st_i32(
             dst[i, j] = src[i, j]
 
 
-_amx_zero_i32 = ("_tile_zero({tile_int});")
-
-
-@instr(_amx_zero_i32)
+@instr('_tile_zero({tile_int});')
 def zero_i32(
         m: size,
         n: size,
@@ -143,6 +144,10 @@ def zero_i32(
         for j in par(0, n):
             tile[i, j] = 0.0
 
+
+# ---------------------------------------------------------------------------- #
+# dpbssd, dpbssd_3d                                                            #
+# ---------------------------------------------------------------------------- #
 
 """
 dpbssd(2, 0, 1) // tile2 = tile0*tile1
