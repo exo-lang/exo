180a23c3ccc2c60fd0662620eefb9311ac4d9ea3
Alex Reinking <alex.reinking@gmail.com>
2022-01-27 21:25:49 -0800

    Format test_precision.py

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -14,40 +14,41 @@ from .helper import TMP_DIR, nprand, generate_lib, nparray, cvt_c
 
 def gen_good_prec1():
     @proc
-    def good_prec1(n : size, m : size,
-                   x : f32[n,m], y: f32[n,m], res : f64[n,m]):
-        for i in par(0,n):
-            rloc : f64[m]
-            xloc : f32[m]
-            yloc : f32[m]
-            for j in par(0,m):
-                xloc[j] = x[i,j]
-            for j in par(0,m):
-                yloc[j] = y[i,j]
-            for j in par(0,m):
+    def good_prec1(n: size, m: size,
+                   x: f32[n, m], y: f32[n, m], res: f64[n, m]):
+        for i in par(0, n):
+            rloc: f64[m]
+            xloc: f32[m]
+            yloc: f32[m]
+            for j in par(0, m):
+                xloc[j] = x[i, j]
+            for j in par(0, m):
+                yloc[j] = y[i, j]
+            for j in par(0, m):
                 rloc[j] = xloc[j] + yloc[j]
-            for j in par(0,m):
-                res[i,j] = rloc[j]
+            for j in par(0, m):
+                res[i, j] = rloc[j]
 
     return good_prec1
+
 
 # Binop on different precision
 def gen_bad_prec1():
     @proc
-    def bad_prec1(n : size, m : size,
-                  x : f32[n,m], y: i8[n,m], res : f64[n,m]):
-        for i in par(0,n):
-            rloc : f64[m]
-            xloc : f32[m]
-            yloc : i8[m]
-            for j in par(0,m):
-                xloc[j] = x[i,j]
-            for j in par(0,m):
-                yloc[j] = y[i,j]
-            for j in par(0,m):
+    def bad_prec1(n: size, m: size,
+                  x: f32[n, m], y: i8[n, m], res: f64[n, m]):
+        for i in par(0, n):
+            rloc: f64[m]
+            xloc: f32[m]
+            yloc: i8[m]
+            for j in par(0, m):
+                xloc[j] = x[i, j]
+            for j in par(0, m):
+                yloc[j] = y[i, j]
+            for j in par(0, m):
                 rloc[j] = xloc[j] + yloc[j]
-            for j in par(0,m):
-                res[i,j] = rloc[j]
+            for j in par(0, m):
+                res[i, j] = rloc[j]
 
     return bad_prec1
 
@@ -79,33 +80,36 @@ def test_good_prec1():
     good_prec1.interpret(n=n_size, m=m_size, x=x, y=y, res=res)
     np.testing.assert_almost_equal(res, res_c)
     np.testing.assert_almost_equal(res_c, nparray(
-        [[3.6, 5.7, 11.9], [4.5, 6.3, 12.0]],typ=np.float64), decimal=4)
+        [[3.6, 5.7, 11.9], [4.5, 6.3, 12.0]], typ=np.float64), decimal=4)
 
 
 def gen_dot():
     @proc
-    def dot(m: size, x : f32[m] , y : f32[m] , r : f32 ):
+    def dot(m: size, x: f32[m], y: f32[m], r: f32):
         r = 0.0
         for i in par(0, m):
-            r += x[i]*y[i]
+            r += x[i] * y[i]
 
     return dot
 
+
 def gen_good_prec2(dot):
     @proc
-    def hoge(n : size, x : f32[n], y : f32[n]):
-        xy : f32
+    def hoge(n: size, x: f32[n], y: f32[n]):
+        xy: f32
         dot(n, x, y, xy)
 
     return hoge
+
 
 def gen_bad_prec2(dot):
     @proc
-    def hoge(n : size, x : i8[n], y : i8[n]):
-        xy : f32
+    def hoge(n: size, x: i8[n], y: i8[n]):
+        xy: f32
         dot(n, x, y, xy)
 
     return hoge
+
 
 def test_bad_prec1():
     with pytest.raises(TypeError,
@@ -113,6 +117,7 @@ def test_bad_prec1():
         bad_prec1 = gen_bad_prec1()
         filename = "test_bad_prec1"
         bad_prec1.compile_c(TMP_DIR, filename)
+
 
 def test_good_prec2():
     dot = gen_dot()
@@ -122,6 +127,7 @@ def test_good_prec2():
     filename = "test_good_prec2"
 
     good_prec2.compile_c(TMP_DIR, filename)
+
 
 def test_bad_prec2():
     with pytest.raises(TypeError,
