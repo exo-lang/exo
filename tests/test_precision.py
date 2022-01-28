833d34f63808a94a3ec2423b9e26e7e2c9295940
Alex Reinking <alex.reinking@gmail.com>
2022-01-27 21:33:05 -0800

    Use new fixtures in test_precision.py

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-import os
-from ctypes import POINTER, c_int
-
 import numpy as np
 import pytest
 
 from SYS_ATL import proc, Procedure
-from .helper import TMP_DIR, nprand, generate_lib, nparray, cvt_c
+from .helper import nprand, nparray
 
 
 # ------- Precision casting tests ------
@@ -53,34 +50,20 @@ def gen_bad_prec1():
     return bad_prec1
 
 
-def test_good_prec1():
+def test_good_prec1(compiler):
     good_prec1 = gen_good_prec1()
     assert isinstance(good_prec1, Procedure)
 
-    filename = "test_good_prec1"
-
-    # Write pretty printing to a file
-    f_pretty = open(os.path.join(TMP_DIR, filename + "_pretty.atl"), "w")
-    f_pretty.write(str(good_prec1))
-    f_pretty.close()
-
-    good_prec1.compile_c(TMP_DIR, filename)
+    fn = compiler.compile(good_prec1)
 
     x = nparray([[1.0, 2.0, 3.0], [3.2, 4.0, 5.3]])
     y = nparray([[2.6, 3.7, 8.9], [1.3, 2.3, 6.7]])
-    n_size = 2
-    m_size = 3
-    res = nprand(size=(n_size, m_size), typ=np.float64)
-    res_c = cvt_c(res, typ=np.float64)
+    res = nprand(size=x.shape, typ=np.float64)
 
-    test_lib = generate_lib(filename)
-    test_lib.good_prec1(POINTER(c_int)(), c_int(n_size), c_int(
-        m_size), cvt_c(x), cvt_c(y), res_c)
-    res_c = np.ctypeslib.as_array(res_c, shape=(n_size, m_size))
-    good_prec1.interpret(n=n_size, m=m_size, x=x, y=y, res=res)
-    np.testing.assert_almost_equal(res, res_c)
-    np.testing.assert_almost_equal(res_c, nparray(
-        [[3.6, 5.7, 11.9], [4.5, 6.3, 12.0]], typ=np.float64), decimal=4)
+    fn(None, *x.shape, x, y, res)
+
+    np.testing.assert_almost_equal(res, nparray(
+        [[3.6, 5.7, 11.9], [4.5, 6.3, 12.0]], typ=np.float64), decimal=6)
 
 
 def gen_dot():
@@ -112,27 +95,25 @@ def gen_bad_prec2(dot):
 
 
 def test_bad_prec1():
+    bad_prec1 = gen_bad_prec1()
+
     with pytest.raises(TypeError,
                        match='Errors occurred during precision checking'):
-        bad_prec1 = gen_bad_prec1()
-        filename = "test_bad_prec1"
-        bad_prec1.compile_c(TMP_DIR, filename)
+        bad_prec1.c_code_str()
 
 
-def test_good_prec2():
+def test_good_prec2(golden):
     dot = gen_dot()
     good_prec2 = gen_good_prec2(dot)
     assert isinstance(good_prec2, Procedure)
 
-    filename = "test_good_prec2"
-
-    good_prec2.compile_c(TMP_DIR, filename)
+    assert good_prec2.c_code_str() == golden
 
 
 def test_bad_prec2():
+    dot = gen_dot()
+    bad_prec2 = gen_bad_prec2(dot)
+
     with pytest.raises(TypeError,
                        match='Errors occurred during precision checking'):
-        dot = gen_dot()
-        bad_prec2 = gen_bad_prec2(dot)
-        filename = "test_bad_prec2"
-        bad_prec2.compile_c(TMP_DIR, filename)
+        bad_prec2.c_code_str()
