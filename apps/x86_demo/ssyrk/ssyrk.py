ccfa5610d2d00a3dcf77abbef6327c437716e9a2
Alex Reinking <alex.reinking@gmail.com>
2022-02-07 02:48:43 -0800

    try to schedule SSYRK with staging

--- apps/x86_demo/ssyrk/ssyrk.py
+++ apps/x86_demo/ssyrk/ssyrk.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from SYS_ATL import *
-from SYS_ATL.platforms.x86 import *
 
 
 # This is the reference code we _actually_ want to schedule.
@@ -17,6 +16,12 @@ def SSYRK_SIMPLE(M: size, K: size, A: f32[M, K], C: f32[M, M]):
             if j >= i:
                 for k in seq(0, K):
                     C[i, j] += A[i, k] * A[j, k]
+
+
+SSYRK_SIMPLE = (
+    SSYRK_SIMPLE
+        # .split('k', 16, ['ko', 'ki'], tail='cut_and_guard')
+)
 
 
 # row-major, upper-triangular, C := C + A @ A^T.
@@ -54,47 +59,48 @@ SSYRK_WINDOW = (
         .set_window('C', True)
 )
 
-ssyrk_edge_kernel = (
-    SSYRK_WINDOW
-        .rename('ssyrk_edge_kernel')
-        .lift_alloc('c_acc: _', n_lifts=3)
-        .expand_dim('c_acc: _', 'M', 'j')
-        .expand_dim('c_acc: _', 'M', 'i')
-        .set_memory('c_acc', AVX512)
-
-        .fission_after('for k in _: _ #0', n_lifts=3)
-        .fission_after('if K % _ > 0: _', n_lifts=3)
-        .replace_all(mm512_set0_ps)
-        .replace_all(mm512_reduce_add_ps)
-
-        .stage_expr('A_vec', 'A[_] #0', memory=AVX512)
-        .stage_expr('At_vec', 'A[_] #1', memory=AVX512)
-        .replace_all(mm512_loadu_ps)
-        .replace_all(mm512_fmadd_ps)
-        .simplify()
-
-        .bound_and_guard('for ki in _: _')
-        .stage_expr('A_vec_mask', 'A[_] #2', memory=AVX512, n_lifts=2)
-        .stage_expr('At_vec_mask', 'A[_] #3', memory=AVX512, n_lifts=2)
-        .replace_all(mm512_maskz_loadu_ps)
-        .replace_all(mm512_mask_fmadd_ps)
-        .simplify()
-
-        .fission_after('for ko in _: _', n_lifts=3)
-        .lift_if('if K % _ > 0: _', n_lifts=3)
-
-        .partial_eval(6)
-
-        # TODO: need a way of hoisting a for past an if. This is a hack!
-        .unroll('i')
-        .unroll('j')
-        .simplify()
-
-        # TODO: slow!!
-        .repeat(Procedure.fuse_loop, 'for ko in _: _ #0', 'for ko in _: _ #1')
-)
+# ssyrk_edge_kernel = (
+#     SSYRK_WINDOW
+#         .rename('ssyrk_edge_kernel')
+#         .lift_alloc('c_acc: _', n_lifts=3)
+#         .expand_dim('c_acc: _', 'M', 'j')
+#         .expand_dim('c_acc: _', 'M', 'i')
+#         .set_memory('c_acc', AVX512)
+#
+#         .fission_after('for k in _: _ #0', n_lifts=3)
+#         .fission_after('if K % _ > 0: _', n_lifts=3)
+#         .replace_all(mm512_set0_ps)
+#         .replace_all(mm512_reduce_add_ps)
+#
+#         .stage_expr('A_vec', 'A[_] #0', memory=AVX512)
+#         .stage_expr('At_vec', 'A[_] #1', memory=AVX512)
+#         .replace_all(mm512_loadu_ps)
+#         .replace_all(mm512_fmadd_ps)
+#         .simplify()
+#
+#         .bound_and_guard('for ki in _: _')
+#         .stage_expr('A_vec_mask', 'A[_] #2', memory=AVX512, n_lifts=2)
+#         .stage_expr('At_vec_mask', 'A[_] #3', memory=AVX512, n_lifts=2)
+#         .replace_all(mm512_maskz_loadu_ps)
+#         .replace_all(mm512_mask_fmadd_ps)
+#         .simplify()
+#
+#         .fission_after('for ko in _: _', n_lifts=3)
+#         .lift_if('if K % _ > 0: _', n_lifts=3)
+#
+#         .partial_eval(6)
+#
+#         # TODO: need a way of hoisting a for past an if. This is a hack!
+#         .unroll('i')
+#         .unroll('j')
+#         .simplify()
+#
+#         # TODO: slow!!
+#         .repeat(Procedure.fuse_loop, 'for ko in _: _ #0', 'for ko in _: _ #1')
+# )
 
 if __name__ == '__main__':
-    print(ssyrk_edge_kernel)
+    print(SSYRK_SIMPLE)
+    print(SSYRK)
 
 __all__ = ['SSYRK', 'ssyrk_edge_kernel']
