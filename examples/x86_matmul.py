2afcdbe4799fd5b7cb90c25a955d5a3e9f6cad38
Yuka Ikarashi <yukatkh@gmail.com>
2022-09-28 18:42:29 -0400

    Update the code, name, and numbers of the examples to accomodate scheduling API change

--- examples/x86_matmul.py
+++ examples/x86_matmul.py
@@ -5,6 +5,7 @@ import sys
 
 from exo import proc
 from exo.platforms.x86 import *
+from exo.stdlib.scheduling import *
 
 # Hide output when running through exocc.
 if __name__ != '__main__' and hasattr(os, 'devnull'):
@@ -32,65 +33,65 @@ print(rank_k_reduce_6x16)
 
 # First block
 """
-avx = rank_k_reduce_6x16.rename("rank_k_reduce_6x16_scheduled")
-avx = avx.stage_assn('C_reg', 'C[_] += _')
-avx = avx.set_memory('C_reg', AVX2)
+avx = rename(rank_k_reduce_6x16, "rank_k_reduce_6x16_scheduled")
+avx = stage_assn(avx, 'C[_] += _', 'C_reg')
+avx = set_memory(avx, 'C_reg', AVX2)
 print("First block:")
 print(avx)
 """
 
 # Second block
 """
-avx = avx.split('j', 8, ['jo', 'ji'], perfect=True)
-avx = avx.reorder('ji', 'k')
-avx = avx.reorder('jo', 'k')
-avx = avx.reorder('i', 'k')
+avx = divide_loop(avx, 'j', 8, ['jo', 'ji'], perfect=True)
+avx = reorder_loops(avx, 'ji k')
+avx = reorder_loops(avx, 'jo k')
+avx = reorder_loops(avx, 'i k')
 print("Second block:")
 print(avx)
 """
 
 # Third block
 """
-avx = avx.lift_alloc('C_reg:_', n_lifts=3)
-avx = avx.fission_after('C_reg = _ #0', n_lifts=3)
-avx = avx.fission_after('C_reg[_] += _ #0', n_lifts=3)
-avx = avx.lift_alloc('C_reg:_', n_lifts=1)
-avx = avx.fission_after('for i in _:_#0', n_lifts=1)
-avx = avx.fission_after('for i in _:_#1', n_lifts=1)
-avx = avx.simplify()
+avx = autolift_alloc(avx, 'C_reg:_', n_lifts=4, keep_dims=True)
+avx = autofission(avx, avx.find('C_reg = _ #0').after(), n_lifts=3)
+avx = autofission(avx, avx.find('C_reg[_] += _ #0').after(), n_lifts=3)
+avx = autofission(avx, avx.find('for i in _:_#0').after(), n_lifts=1)
+avx = autofission(avx, avx.find('for i in _:_#1').after(), n_lifts=1)
+avx = simplify(avx)
 print("Third block:")
 print(avx)
 """
 
 # Fourth block
 """
-avx = avx.bind_expr('a_vec', 'A[i, k]')
-avx = avx.set_memory('a_vec', AVX2)
-avx = avx.lift_alloc('a_vec:_', keep_dims=True)
-avx = avx.fission_after('a_vec[_] = _')
+avx = bind_expr(avx, 'A[i, k]', 'a_vec')
+avx = set_memory(avx, 'a_vec', AVX2)
+avx = expand_dim(avx, 'a_vec:_', '8', 'ji')
+avx = autolift_alloc(avx, 'a_vec:_')
+avx = autofission(avx, avx.find('a_vec[_] = _').after())
 print("Fourth block:")
 print(avx)
 """
 
 # Fifth block
 """
-avx = avx.bind_expr('b_vec', 'B[k, _]')
-avx = avx.set_memory('b_vec', AVX2)
-avx = avx.lift_alloc('b_vec:_', keep_dims=True)
-avx = avx.fission_after('b_vec[_] = _')
+avx = bind_expr(avx, 'B[k, _]', 'b_vec')
+avx = set_memory(avx, 'b_vec', AVX2)
+avx = autolift_alloc(avx, 'b_vec:_', keep_dims=True)
+avx = autofission(avx, avx.find('b_vec[_] = _').after())
 print("Fifth block:")
 print(avx)
 """
 
 # Sixth block
 """
-avx = avx.replace_all(avx2_set0_ps)
-avx = avx.replace_all(mm256_broadcast_ss)
-avx = avx.replace_all(mm256_fmadd_ps)
-avx = avx.replace_all(avx2_fmadd_memu_ps)
-avx = avx.replace(mm256_loadu_ps, 'for ji in _:_ #0')
-avx = avx.replace(mm256_loadu_ps, 'for ji in _:_ #0')
-avx = avx.replace(mm256_storeu_ps, 'for ji in _:_ #0')
+avx = replace_all(avx, avx2_set0_ps)
+avx = replace_all(avx, mm256_broadcast_ss)
+avx = replace_all(avx, mm256_fmadd_ps)
+avx = replace_all(avx, avx2_fmadd_memu_ps)
+avx = replace(avx, 'for ji in _:_ #0', mm256_loadu_ps)
+avx = replace(avx, 'for ji in _:_ #0', mm256_loadu_ps)
+avx = replace(avx, 'for ji in _:_ #0', mm256_storeu_ps)
 print("Sixth block:")
 print(avx)
 """
