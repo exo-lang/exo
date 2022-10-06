c50f6e9f5bc3cd7db6d34284936cb2cef6b5338c
Alex Reinking <alex_reinking@berkeley.edu>
2022-10-06 00:52:48 -0400

    Add GEMMINI_BAREMETAL guard

--- apps/gemmini/cmake/gemmini.cmake
+++ apps/gemmini/cmake/gemmini.cmake
@@ -1,16 +1,18 @@
 cmake_minimum_required(VERSION 3.22)
 
-set(RISCV "$ENV{RISCV}" CACHE PATH "Path to RISCV toolchain")
-set(ARCH riscv64-unknown-elf)
-
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+set(RISCV "$ENV{RISCV}" CACHE PATH "Path to RISCV toolchain")
 
 set(CMAKE_SYSROOT "${RISCV}/sysroot")
 set(CMAKE_CROSSCOMPILING_EMULATOR "${RISCV}/bin/spike" --extension=gemmini)
 
-set(CMAKE_C_COMPILER "${RISCV}/bin/${ARCH}-gcc")
-set(CMAKE_CXX_COMPILER "${RISCV}/bin/${ARCH}-g++")
+set(CMAKE_C_COMPILER "${RISCV}/bin/riscv64-unknown-elf-gcc")
+set(CMAKE_CXX_COMPILER "${RISCV}/bin/riscv64-unknown-elf-g++")
 
-set(CMAKE_C_FLAGS_INIT "-mcmodel=medany -nostartfiles -nostdlib -fno-tree-loop-distribute-patterns -fno-builtin-printf -fno-common")
-set(CMAKE_C_FLAGS_RELEASE "-O2 -DNDEBUG")
+option(GEMMINI_BAREMETAL "Compile for bare-metal Gemmini" ON)
+if (GEMMINI_BAREMETAL)
+    set(CMAKE_C_FLAGS_INIT "-mcmodel=medany -fno-tree-loop-distribute-patterns -fno-builtin-printf -fno-common")
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostartfiles -nostdlib")
+endif ()
