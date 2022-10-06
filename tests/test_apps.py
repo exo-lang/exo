5a3472262b546069e295ca499ea54b98da15b995
Alex Reinking <alex_reinking@berkeley.edu>
2022-10-06 14:28:23 -0400

    Turn Gemmini tests into app tests (like x86/neon)

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -8,7 +8,8 @@ import exo.main
 REPO_ROOT = Path(__file__).parent.parent.resolve()
 
 
-def _test_app(module_file):
+def _test_app(module_file: Path):
+    module_file = module_file.resolve(strict=True)
     mod = exo.main.load_user_code(module_file)
     procs = exo.main.get_procs_from_module(mod)
 
@@ -17,16 +18,28 @@ def _test_app(module_file):
     return f'{h_file}\n{c_file}'
 
 
+# ---------------------------------------------------------------------------- #
+
 def test_x86_sgemm(golden):
     module_file = REPO_ROOT / 'apps' / 'x86' / 'sgemm' / 'sgemm.py'
-    assert _test_app(module_file.resolve(strict=True)) == golden
+    assert _test_app(module_file) == golden
 
 
 def test_x86_conv(golden):
     module_file = REPO_ROOT / 'apps' / 'x86' / 'conv' / 'conv.py'
-    assert _test_app(module_file.resolve(strict=True)) == golden
+    assert _test_app(module_file) == golden
 
 
 def test_neon_sgemm(golden):
     module_file = REPO_ROOT / 'apps' / 'aarch64' / 'sgemm' / 'sgemm.py'
-    assert _test_app(module_file.resolve(strict=True)) == golden
+    assert _test_app(module_file) == golden
+
+
+def test_gemmini_matmul(golden):
+    module_file = REPO_ROOT / 'apps' / 'gemmini' / 'src' / 'exo' / 'matmul.py'
+    assert _test_app(module_file) == golden
+
+
+def test_gemmini_conv(golden):
+    module_file = REPO_ROOT / 'apps' / 'gemmini' / 'src' / 'exo' / 'conv.py'
+    assert _test_app(module_file) == golden
