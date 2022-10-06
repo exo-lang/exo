4bbcb27532881c71306f35b44a2f9d35389c4d30
Alex Reinking <alex_reinking@berkeley.edu>
2022-10-05 22:52:01 -0400

    add_exo_library: support multiple source files

--- src/exo/cmake/AddExoLibrary.cmake
+++ src/exo/cmake/AddExoLibrary.cmake
@@ -1,17 +1,23 @@
-function(add_exo_library target source_file)
-  cmake_path(ABSOLUTE_PATH source_file
-             BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-             NORMALIZE)
+function(add_exo_library target)
+  set(source_files "")
+
+  foreach (src IN LISTS ARGN)
+    cmake_path(ABSOLUTE_PATH src
+               BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+               NORMALIZE)
+    list(APPEND source_files "${src}")
+  endforeach ()
 
   set(intdir "${CMAKE_CURRENT_BINARY_DIR}/${target}.exo")
   set(files "${intdir}/${target}.c" "${intdir}/${target}.h")
 
   add_custom_command(
     OUTPUT ${files}
-    COMMAND Exo::compiler -o "${intdir}" --stem "${target}" "${source_file}"
-    DEPENDS "${source_file}"
+    COMMAND Exo::compiler -o "${intdir}" --stem "${target}" ${source_files}
+    DEPENDS ${source_files}
     VERBATIM
   )
+
   add_library(${target} ${files})
   add_library(${PROJECT_NAME}::${target} ALIAS ${target})
   target_include_directories(${target} PUBLIC "$<BUILD_INTERFACE:${intdir}>")
