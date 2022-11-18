dc98d121d0b6b39985947bac7402c73d1cdcb111
Alex Reinking <alex.reinking@gmail.com>
2022-11-17 23:05:38 -0500

    Fix up syntax.py

--- src/exo/syntax.py
+++ src/exo/syntax.py
@@ -3,12 +3,26 @@ This module defines special symbols that are used inside Exo code. You may
 import this module via `from exo.syntax import *` to suppress warnings and
 see documentation inside an IDE (like PyCharm).
 """
+from __future__ import annotations
 
-from typing import TypeVar as _TypeVar
+from typing import TypeVar
+
+# fmt: off
+
+# With so many tiny names, letting Black put them all one-to-a-line is beyond hideous
+__all__ = [
+    "size", "index", "i8", "i32", "f32", "stride", "seq", "D", "E", "F", "G", "H", "I",
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"
+]
+
+
+# fmt: on
 
 
 def _fail_dummy():
-    raise SyntaxError("Exo Python dummy functions should never be called " "directly.")
+    raise SyntaxError("Exo Python dummy functions should never be called directly.")
 
 
 # noinspection PyPep8Naming
@@ -18,7 +32,37 @@ class size(int):
     """
 
     def __init__(self, *_):
-        raise SyntaxError("Exo Python dummy objects should never be " "instantiated")
+        raise SyntaxError("Exo Python dummy objects should never be instantiated")
+
+
+# noinspection PyPep8Naming
+class index(int):
+    """
+    A type representing a index in Exo.
+    """
+
+    def __init__(self, *_):
+        raise SyntaxError("Exo Python dummy objects should never be instantiated")
+
+
+# noinspection PyPep8Naming
+class i8(float):
+    """
+    A type representing a 32-bit integer value.
+    """
+
+    def __init__(self, *_):
+        raise SyntaxError("Exo Python dummy objects should never be instantiated")
+
+
+# noinspection PyPep8Naming
+class i32(float):
+    """
+    A type representing a 32-bit integer value.
+    """
+
+    def __init__(self, *_):
+        raise SyntaxError("Exo Python dummy objects should never be instantiated")
 
 
 # noinspection PyPep8Naming
@@ -28,7 +72,7 @@ class f32(float):
     """
 
     def __init__(self, *_):
-        raise SyntaxError("Exo Python dummy objects should never be " "instantiated")
+        raise SyntaxError("Exo Python dummy objects should never be instantiated")
 
 
 def stride(tensor, dimension):
@@ -39,10 +83,10 @@ def stride(tensor, dimension):
     _fail_dummy()
 
 
-def par(lo, hi):
+def seq(lo, hi):
     """
-    Parallel range between lo (must be 0) and hi (exclusive). May only be
-    used in a for-loop bound. Iterations proceed as if in random order.
+    Iteration range between lo (must be 0) and hi (exclusive). May only be
+    used in a for-loop bound.
     """
 
 
@@ -50,54 +94,54 @@ def par(lo, hi):
 # A = _TypeVar('A', bound=size)
 # B = _TypeVar('B', bound=size)
 # C = _TypeVar('C', bound=size)
-D = _TypeVar("D", bound=size)
-E = _TypeVar("E", bound=size)
-F = _TypeVar("F", bound=size)
-G = _TypeVar("G", bound=size)
-H = _TypeVar("H", bound=size)
-I = _TypeVar("I", bound=size)
-J = _TypeVar("J", bound=size)
-K = _TypeVar("K", bound=size)
-L = _TypeVar("L", bound=size)
-M = _TypeVar("M", bound=size)
-N = _TypeVar("N", bound=size)
-O = _TypeVar("O", bound=size)
-P = _TypeVar("P", bound=size)
-Q = _TypeVar("Q", bound=size)
-R = _TypeVar("R", bound=size)
-S = _TypeVar("S", bound=size)
-T = _TypeVar("T", bound=size)
-U = _TypeVar("U", bound=size)
-V = _TypeVar("V", bound=size)
-W = _TypeVar("W", bound=size)
-X = _TypeVar("X", bound=size)
-Y = _TypeVar("Y", bound=size)
-Z = _TypeVar("Z", bound=size)
+D = TypeVar("D", bound=size)
+E = TypeVar("E", bound=size)
+F = TypeVar("F", bound=size)
+G = TypeVar("G", bound=size)
+H = TypeVar("H", bound=size)
+I = TypeVar("I", bound=size)
+J = TypeVar("J", bound=size)
+K = TypeVar("K", bound=size)
+L = TypeVar("L", bound=size)
+M = TypeVar("M", bound=size)
+N = TypeVar("N", bound=size)
+O = TypeVar("O", bound=size)
+P = TypeVar("P", bound=size)
+Q = TypeVar("Q", bound=size)
+R = TypeVar("R", bound=size)
+S = TypeVar("S", bound=size)
+T = TypeVar("T", bound=size)
+U = TypeVar("U", bound=size)
+V = TypeVar("V", bound=size)
+W = TypeVar("W", bound=size)
+X = TypeVar("X", bound=size)
+Y = TypeVar("Y", bound=size)
+Z = TypeVar("Z", bound=size)
 
-a = _TypeVar("a", bound=size)
-b = _TypeVar("b", bound=size)
-c = _TypeVar("c", bound=size)
-d = _TypeVar("d", bound=size)
-e = _TypeVar("e", bound=size)
-f = _TypeVar("f", bound=size)
-g = _TypeVar("g", bound=size)
-h = _TypeVar("h", bound=size)
+a = TypeVar("a", bound=size)
+b = TypeVar("b", bound=size)
+c = TypeVar("c", bound=size)
+d = TypeVar("d", bound=size)
+e = TypeVar("e", bound=size)
+f = TypeVar("f", bound=size)
+g = TypeVar("g", bound=size)
+h = TypeVar("h", bound=size)
 # Common loop variable names:
 # i = _TypeVar('i', bound=size)
 # j = _TypeVar('j', bound=size)
 # k = _TypeVar('k', bound=size)
-l = _TypeVar("l", bound=size)
-m = _TypeVar("m", bound=size)
-n = _TypeVar("n", bound=size)
-o = _TypeVar("o", bound=size)
-p = _TypeVar("p", bound=size)
-q = _TypeVar("q", bound=size)
-r = _TypeVar("r", bound=size)
-s = _TypeVar("s", bound=size)
-t = _TypeVar("t", bound=size)
-u = _TypeVar("u", bound=size)
-v = _TypeVar("v", bound=size)
-w = _TypeVar("w", bound=size)
-x = _TypeVar("x", bound=size)
-y = _TypeVar("y", bound=size)
-z = _TypeVar("z", bound=size)
+l = TypeVar("l", bound=size)
+m = TypeVar("m", bound=size)
+n = TypeVar("n", bound=size)
+o = TypeVar("o", bound=size)
+p = TypeVar("p", bound=size)
+q = TypeVar("q", bound=size)
+r = TypeVar("r", bound=size)
+s = TypeVar("s", bound=size)
+t = TypeVar("t", bound=size)
+u = TypeVar("u", bound=size)
+v = TypeVar("v", bound=size)
+w = TypeVar("w", bound=size)
+x = TypeVar("x", bound=size)
+y = TypeVar("y", bound=size)
+z = TypeVar("z", bound=size)
