bda71d3624cdb3b94d14fa2bb0292dea0c09224a
Alex Reinking <alex.reinking@gmail.com>
2022-11-17 22:12:21 -0500

    Optimize imports in __init__.py. Add TODO for order-sensitive import.

--- src/exo/__init__.py
+++ src/exo/__init__.py
@@ -1,11 +1,11 @@
 from .API import Procedure, compile_procs, compile_procs_to_strings, proc, instr, config
 from .LoopIR_scheduling import SchedulingError
-from .parse_fragment import ParseFragmentError
 from .configs import Config
 from .memory import Memory, DRAM
-from . import query_asts as QAST
+from .parse_fragment import ParseFragmentError
 
-from . import stdlib
+# TODO: this import must come after .configs!
+import exo.query_asts as QAST
 
 __version__ = "0.0.2"
 
@@ -22,6 +22,4 @@ __all__ = [
     "QAST",
     "SchedulingError",
     "ParseFragmentError",
-    #
-    "stdlib",
 ]
