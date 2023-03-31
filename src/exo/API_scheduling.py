8e5d70bc920b932e526b6c32640cafd05b2f644b
Kevin Qian <keqian@mit.edu>
2023-03-30 23:25:35 -0400

    implements divide_loop with internal cursors

--- src/exo/API_scheduling.py
+++ src/exo/API_scheduling.py
@@ -1457,17 +1457,16 @@ def divide_loop(proc, loop_cursor, div_const, new_iters, tail="guard", perfect=F
         raise ValueError("why are you trying to split by 1?")
 
     stmt = loop_cursor._impl
-    proc_c = ic.Cursor.create(proc)
 
-    return scheduling.DoSplit(
-        proc_c,
+    ir, _fwd = scheduling.DoSplit(
         stmt,
         quot=div_const,
         hi=new_iters[0],
         lo=new_iters[1],
         tail=tail,
         perfect=perfect,
-    ).result()
+    )
+    return Procedure(ir, _provenance_eq_Procedure=proc)
 
 
 @sched_op([NestedForSeqCursorA, NameA])
--- tests/test_schedules.py
+++ tests/test_schedules.py
@@ -873,6 +873,17 @@ def test_simple_divide_loop(golden):
     assert str(bar) == golden
 
 
+def test_divide_loop_cut_and_guard(golden):
+    @proc
+    def bar(n: size, A: i8[n]):
+        tmp: i8[n]
+        for i in seq(0, n):
+            tmp[i] = A[i]
+
+    bar = divide_loop(bar, "i", 4, ["io", "ii"], tail="cut_and_guard")
+    assert str(bar) == golden
+
+
 def test_simple_reorder(golden):
     @proc
     def bar(n: size, m: size, A: i8[n, m]):
