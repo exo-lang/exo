ffc8e6c144b11f852beb59096e4e1e316c806b43
Yuka Ikarashi <yukatkh@gmail.com>
2023-07-19 22:24:56 -0700

    Implement div denominator constant folding to simplify (#473)
    Closes #469
    
    ---------
    
    Co-authored-by: Kevin Qian <keqian@mit.edu>

--- tests/test_schedules.py
+++ tests/test_schedules.py
@@ -319,6 +319,26 @@ def test_simplify_loop_bounds(golden):
     assert str(simplify(foo)) == golden
 
 
+def test_simplify_nested_div(golden):
+    @proc
+    def foo(n: size):
+        x: f32
+        for i in seq(0, (n / 4) / 3 / 2):
+            x = 0.0
+
+    assert str(simplify(foo)) == golden
+
+
+def test_simplify_nested_div_2(golden):
+    @proc
+    def foo(n: size):
+        x: f32
+        for i in seq(0, ((3 * n) / 4) / 3):
+            x = 0.0
+
+    assert str(simplify(foo)) == golden
+
+
 def test_pattern_match():
     @proc
     def foo(N1: size, M1: size, K1: size, N2: size, M2: size, K2: size):
