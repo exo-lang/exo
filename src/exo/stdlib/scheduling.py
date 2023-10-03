32335fbcaca0c25fddda5765c15c581b7576732d
Kevin Qian <keqian@mit.edu>
2023-10-03 11:40:20 -0400

    moved store_at and compute_at to scheduling.py

--- src/exo/stdlib/scheduling.py
+++ src/exo/stdlib/scheduling.py
@@ -316,3 +316,71 @@ def lift_if(proc, cursor, n_lifts=1):
                 proc=proc,
             ) from e
     return proc
+
+
+def compute_at(proc, proc_name):
+    blur = rename(proc, proc_name)
+
+    prod_loop = blur.find_loop("i")
+    consumer_loop = blur.find_loop("i #1")
+    accesses = range(2)  # TODO: need introspection to get this
+
+    # Assumes constant, consecutive windows
+    first_prod_loop = prod_loop
+    for i in accesses[1:]:
+        # surgery
+        blur = cut_loop(blur, prod_loop, f"n + {i} - 1")
+        blur = cut_loop(blur, prod_loop, i)
+
+        # duplicate work
+        middle_prod_loop = blur.forward(prod_loop).next()
+        blur = add_loop(blur, middle_prod_loop, "ii", 2)
+        blur = unroll_loop(blur, blur.forward(middle_prod_loop).parent())
+
+        # stitch together
+        blur = join_loops(blur, prod_loop, blur.forward(prod_loop).next())
+        next_loop = blur.forward(prod_loop).next()
+        blur = join_loops(blur, next_loop, next_loop.next())
+        blur = simplify(blur)
+        prod_loop = next_loop
+
+    # merge producer loops
+    prod_loop = blur.forward(first_prod_loop)
+    for i in accesses[1:]:
+        next_loop = prod_loop.next()
+        blur = shift_loop(blur, next_loop, 0)
+        blur = fuse(blur, prod_loop, next_loop, unsafe_disable_check=True)
+        prod_loop = blur.forward(prod_loop)
+
+    # fuse with consumer
+    blur = fuse(blur, prod_loop, consumer_loop, unsafe_disable_check=True)
+
+    return simplify(blur)
+
+
+def store_at(proc, producer, consumer, loop, proc_name):
+    blur = rename(proc, proc_name)
+
+    producer_alloc = blur.find(f"{producer}:_")
+    consumer_assign = blur.find(f"{consumer} = _")
+    loop = blur.forward(loop)  # need to forward because rename has a fwding function
+
+    name = producer_alloc.name()
+    bound = 2  # TODO: need bounds inference to figure this out
+
+    before_consumer = consumer_assign.prev().as_block().expand(delta_hi=0)
+    blur = stage_mem(blur, before_consumer, f"{name}[i:i+{bound}]", f"{name}_tmp")
+    blur = simplify(blur)
+
+    blur = sink_alloc(blur, producer_alloc)
+
+    blur = unroll_loop(blur, blur.forward(consumer_assign).prev())
+    blur = simplify(blur)
+
+    for i in range(bound):
+        block = blur.forward(consumer_assign).expand(delta_lo=1, delta_hi=0)
+        blur = inline_assign(blur, block)
+
+    blur = delete_buffer(blur, producer_alloc)
+
+    return simplify(blur)
