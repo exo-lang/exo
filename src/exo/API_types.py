93b0953814cc49aa791036a9f24573bdd55e067a
Samir Droubi <samirdr@mit.edu>
2024-02-20 13:22:49 -0500

    Accept API types as arguments to primitives (#580)
    * Type-coerse the API types to the internal types.
    * Add missing internal data types to the API types.

--- src/exo/API_types.py
+++ src/exo/API_types.py
@@ -6,9 +6,12 @@ class ProcedureBase:
 
 
 class ExoType(Enum):
+    F16 = auto()
     F32 = auto()
     F64 = auto()
+    UI8 = auto()
     I8 = auto()
+    UI16 = auto()
     I32 = auto()
     R = auto()
     Index = auto()
@@ -20,7 +23,16 @@ class ExoType(Enum):
         return self in [ExoType.Index, ExoType.Size, ExoType.Int]
 
     def is_numeric(self):
-        return self in [ExoType.F32, ExoType.F64, ExoType.I8, ExoType.I32, ExoType.R]
+        return self in [
+            ExoType.F16,
+            ExoType.F32,
+            ExoType.F64,
+            ExoType.I8,
+            ExoType.UI8,
+            ExoType.UI16,
+            ExoType.I32,
+            ExoType.R,
+        ]
 
     def is_bool(self):
         return self == ExoType.Bool
--- tests/test_schedules.py
+++ tests/test_schedules.py
@@ -7,6 +7,7 @@ from exo import proc, DRAM, Procedure, config
 from exo.libs.memories import GEMM_SCRATCH
 from exo.stdlib.scheduling import *
 from exo.platforms.x86 import *
+from exo.API_types import *
 
 
 def test_commute(golden):
@@ -1780,6 +1781,39 @@ def test_set_precision_for_tensors_and_windows():
     )
     assert str(foo.forward(call1)._impl._node.args[1].type) == "f32[n]"
     assert str(foo.forward(call2)._impl._node.args[1].type) == "f32[n]"
+
+
+def test_set_precision_api_type(golden):
+    @proc
+    def bar(n: size, x: R[n]):
+        pass
+
+    bar = set_precision(bar, bar.args()[1], ExoType.F32)
+    assert str(bar) == golden
+
+
+def test_set_precision_illegal_precision_value():
+    @proc
+    def bar(n: size, x: R[n]):
+        pass
+
+    with pytest.raises(
+        ValueError,
+        match="expected an instance of <enum 'ExoType'> or one of the following strings",
+    ):
+        bar = set_precision(bar, bar.args()[1], "Z")
+
+
+def test_set_precision_illegal_precision_type():
+    @proc
+    def bar(n: size, x: R[n]):
+        pass
+
+    with pytest.raises(
+        TypeError,
+        match="expected an instance of <enum 'ExoType'> or <class 'str'> specifying the precision",
+    ):
+        bar = set_precision(bar, bar.args()[1], bar)
 
 
 def test_rewrite_expr(golden):
