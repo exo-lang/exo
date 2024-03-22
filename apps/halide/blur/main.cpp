eb1db7e49a21876adb1f705f81715433bb89a9fc
Kevin Qian <keqian@mit.edu>
2024-03-22 13:42:03 -0400

    fixed memory leaks and added output checking

--- apps/halide/blur/main.cpp
+++ apps/halide/blur/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <png.h>
 #include <vector>
+#include <memory>
 
 bool read_png_file(const char *filename, std::vector<uint16_t> &buffer,
     int &width, int &height) {
@@ -98,15 +99,12 @@ bool write_png_file(
 typedef void (*blurtype)(void *ctxt, int_fast32_t n, int_fast32_t m,
     uint16_t *g, const uint16_t *inp);
 
-int exec_parrot(blurtype func, std::string output_name, int width, int height,
-    uint16_t *parrot) {
-  uint16_t *parrot_write =
-      (uint16_t *)malloc(sizeof(uint16_t) * width * height);
-
+int exec_blur(blurtype func, std::string output_name, int width, int height,
+    uint16_t *img_in, uint16_t *img_out) {
   auto start = std::chrono::steady_clock::now();
   int iterations = 100;
-  for (int i = 0; i < iterations; i += 4) {
-    func(nullptr, width, height, parrot_write, parrot);
+  for (int i = 0; i < iterations; i++) {
+    func(nullptr, width, height, img_out, img_in);
   }
   auto stop = std::chrono::steady_clock::now();
   float time = (float)std::chrono::duration_cast<std::chrono::microseconds>(
@@ -115,7 +113,7 @@ int exec_parrot(blurtype func, std::string output_name, int width, int height,
   printf("%s: %f microseconds\n", output_name.c_str(), time);
 
   std::string file_name = output_name + std::string(".png");
-  if (!write_png_file(file_name.c_str(), parrot_write, width, height)) {
+  if (!write_png_file(file_name.c_str(), img_out, width, height)) {
     std::cerr << "Error writing PNG file." << std::endl;
   }
 
@@ -131,15 +129,29 @@ int main() {
     printf("width: %d\n", (int)width);
     printf("height: %d\n", (int)height);
 
-    uint16_t *parrot;
-    parrot = (uint16_t *)malloc(sizeof(uint16_t) * (width + 2) * (height + 2));
+    std::vector<uint16_t> img_in ((width + 2) * (height + 2));
     for (int i = 0; i < height; i++) {
-      memcpy(&parrot[(i + 1) * (width + 2) + 1], &buffer[i * width],
+      memcpy(&img_in[(i + 1) * (width + 2) + 1], &buffer[i * width],
           sizeof(uint16_t) * width);
     }
 
-    exec_parrot(blur, "blur", width, height, parrot);
-    exec_parrot(exo_blur_halide, "exo_blur_halide", width, height, parrot);
+    std::vector<uint16_t> ref_img_out (width * height);
+    std::vector<uint16_t> img_out (width * height);
+    exec_blur(blur, "blur", width, height, img_in.data(), ref_img_out.data());
+    exec_blur(exo_blur_halide, "exo_blur_halide", width, height, img_in.data(), img_out.data());
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        if (ref_img_out[y * width + x] != img_out[y * width + x]) {
+          std::cerr << "Mismatch at (" << x << ", " << y << "): "
+                    << ref_img_out[y * width + x] << " != " << img_out[y * width + x]
+                    << std::endl;
+          return 1;
+        }
+      }
+    }
+
+    std::cout << "Results match!" << std::endl;
   } else {
     std::cerr << "Error reading PNG file." << std::endl;
   }
