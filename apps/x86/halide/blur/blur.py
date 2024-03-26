4c372af077781f0d69b5bba45ef5c81fa44c6a0d
Kevin Qian <keqian@mit.edu>
2024-03-26 18:20:46 -0400

    Cleaning up scheduling implementation

--- apps/x86/halide/blur/blur.py
+++ apps/x86/halide/blur/blur.py
@@ -10,23 +10,6 @@ from exo.stdlib.inspection import get_enclosing_loop_by_name
 from exo.stdlib.stdlib import vectorize, is_div, is_literal
 
 
-def halide_tile(p, buffer, y, x, yi, xi, yTile, xTile):
-    assign = p.find(f"{buffer} = _")
-    y_loop = get_enclosing_loop_by_name(p, assign, y)
-    x_loop = get_enclosing_loop_by_name(p, assign, x)
-
-    return tile(p, y_loop, x_loop, [y, yi], [x, xi], yTile, xTile, perfect=True)
-
-
-def halide_compute_at(p, producer: str, consumer: str, loop: str):
-    x_loop = get_enclosing_loop_by_name(p, p.find(f"{consumer} = _"), loop)
-    return compute_at(p, producer, consumer, x_loop)
-
-
-def halide_parallel(p, loop: str):
-    return parallelize_loop(p, p.find_loop(loop))
-
-
 avx_ui16_insts = [
     mm256_loadu_si256,
     mm256_storeu_si256,
@@ -45,15 +28,28 @@ def divide_by_3_rule(proc, expr):
 def halide_vectorize(p, buffer: str, loop: str, width: int):
     loop = get_enclosing_loop_by_name(p, p.find(f"{buffer} = _"), loop)
     rules = [divide_by_3_rule]
+
+    # Ensure that it is the innermost loop
+    while len(loop.body()) == 1 and isinstance(loop.body()[0], ForCursor):
+        p = reorder_loops(p, loop)
+        loop = p.forward(loop)
+
     p = vectorize(
-        p, loop, width, "ui16", AVX2, avx_ui16_insts, rules=rules, tail="perfect"
+        p,
+        loop,
+        width,
+        "ui16",
+        AVX2,
+        avx_ui16_insts,
+        rules=[divide_by_3_rule],
+        tail="perfect",
     )
 
     return p
 
 
 @proc
-def blur(W: size, H: size, blur_y: ui16[H, W], inp: ui16[H + 2, W + 2]):
+def exo_base_blur(W: size, H: size, blur_y: ui16[H, W], inp: ui16[H + 2, W + 2]):
     assert H % 32 == 0
     assert W % 256 == 0
 
@@ -66,18 +62,16 @@ def blur(W: size, H: size, blur_y: ui16[H, W], inp: ui16[H + 2, W + 2]):
             blur_y[y, x] = (blur_x[y, x] + blur_x[y + 1, x] + blur_x[y + 2, x]) / 3.0
 
 
-def prod_halide(p):
+def halide_schedule(p):
     p = halide_tile(p, "blur_y", "y", "x", "yi", "xi", 32, 256)
-    p = halide_compute_at(p, "blur_x", "blur_y", "x")
+    p = halide_compute_and_store_at(p, "blur_x", "blur_y", "x")
     p = halide_parallel(p, "y")
     p = halide_vectorize(p, "blur_x", "xi", 16)
     p = halide_vectorize(p, "blur_y", "xi", 16)
-    p = set_memory(p, p.find(f"blur_x : _"), DRAM_STACK)
-    p = simplify(
-        p
-    )  # necessary because unification is not deterministic, which breaks test cases
+    p = set_memory(p, p.find("blur_x : _"), DRAM_STACK)
+    p = simplify(p)  # necessary because unification is not deterministic
     return p
 
 
-blur_halide = rename(prod_halide(blur), "exo_blur_halide")
-print(blur_halide)
+exo_blur = rename(halide_schedule(exo_base_blur), "exo_blur_halide")
+print(exo_blur)
