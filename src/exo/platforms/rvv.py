54dbc2e16ceda8987c9f1be910f7f8dbfd63a13f
Adrián Castelló <adcastel@disca.upv.es>
2024-07-16 12:42:40 +0200

    support for 256bits

--- src/exo/platforms/rvv.py
+++ src/exo/platforms/rvv.py
@@ -15,7 +15,6 @@ def _is_some_const_size(sz):
 #   Neon registers
 # --------------------------------------------------------------------------- #
 
-
 class RVV(Memory):
     @classmethod
     def global_(cls):
@@ -103,12 +102,24 @@ def rvv_vld_8xf32(dst: [f32][8] @ RVV, src: [f32][8] @ DRAM, vl: size):
     for i in seq(0, vl):
         dst[i] = src[i]
 
+
+
 @instr("__riscv_vse32_v_f32m1(&{dst_data}, {src_data},{vl});")
 def rvv_vst_4xf32(dst: [f32][4] @ DRAM, src: [f32][4] @ RVV, vl: size):
     assert stride(src, 0) == 1
     assert stride(dst, 0) == 1
     assert vl >= 0
     assert vl <= 4
+
+    for i in seq(0, vl):
+        dst[i] = src[i]
+
+@instr("__riscv_vse32_v_f32m1(&{dst_data}, {src_data},{vl});")
+def rvv_vst_8xf32(dst: [f32][8] @ DRAM, src: [f32][8] @ RVV, vl: size):
+    assert stride(src, 0) == 1
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
 
     for i in seq(0, vl):
         dst[i] = src[i]
@@ -123,6 +134,14 @@ def rvv_broadcast_4xf32(dst: [f32][4] @ RVV, src: [f32][1] @ DRAM, vl: size):
     for i in seq(0, vl):
         dst[i] = src[0]
 
+@instr("{dst_data} = __riscv_vfmv_v_f_f32m1({src_data},{vl});")
+def rvv_broadcast_8xf32(dst: [f32][8] @ RVV, src: [f32][1] @ DRAM, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] = src[0]
 
 @instr("{dst_data} = __riscv_vfmv_v_f_f32m1({src_data},{vl});")
 def rvv_broadcast_4xf32_scalar(dst: [f32][4] @ RVV, src: f32 @ DRAM, vl: size):
@@ -133,6 +152,14 @@ def rvv_broadcast_4xf32_scalar(dst: [f32][4] @ RVV, src: f32 @ DRAM, vl: size):
     for i in seq(0, vl):
         dst[i] = src
 
+@instr("{dst_data} = __riscv_vfmv_v_f_f32m1({src_data},{vl});")
+def rvv_broadcast_8xf32_scalar(dst: [f32][8] @ RVV, src: f32 @ DRAM, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] = src
 
 @instr("{dst_data} = __riscv_vfmv_v_f_f32m1(0.0f,{vl});")
 def rvv_broadcast_4xf32_0(dst: [f32][4] @ RVV, vl: size):
@@ -143,6 +170,14 @@ def rvv_broadcast_4xf32_0(dst: [f32][4] @ RVV, vl: size):
     for i in seq(0, vl):
         dst[i] = 0.0
 
+@instr("{dst_data} = __riscv_vfmv_v_f_f32m1(0.0f,{vl});")
+def rvv_broadcast_8xf32_0(dst: [f32][8] @ RVV, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] = 0.0
 
 @instr("{dst_data} = __riscv_vfmacc_vv_f32m1({dst_data}, {lhs_data}, {rhs_data},{vl});")
 def rvv_vfmacc_4xf32_4xf32(
@@ -157,6 +192,18 @@ def rvv_vfmacc_4xf32_4xf32(
     for i in seq(0, vl):
         dst[i] += lhs[i] * rhs[i]
 
+@instr("{dst_data} = __riscv_vfmacc_vv_f32m1({dst_data}, {lhs_data}, {rhs_data},{vl});")
+def rvv_vfmacc_8xf32_8xf32(
+    dst: [f32][8] @ RVV, lhs: [f32][8] @ RVV, rhs: [f32][8] @ RVV, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] += lhs[i] * rhs[i]
 
 @instr("{dst_data} = __riscv_vfmacc_vf_f32m1{dst_data}, {rhs_data}, {lhs_data},{vl});")
 def rvv_vfmacc_4xf32_1xf32(
@@ -171,6 +218,18 @@ def rvv_vfmacc_4xf32_1xf32(
     for i in seq(0, vl):
         dst[i] += lhs[i] * rhs[0]
 
+@instr("{dst_data} = __riscv_vfmacc_vf_f32m1{dst_data}, {rhs_data}, {lhs_data},{vl});")
+def rvv_vfmacc_8xf32_1xf32(
+    dst: [f32][8] @ RVV, lhs: [f32][8] @ RVV, rhs: [f32][1] @ DRAM, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] += lhs[i] * rhs[0]
 
 @instr("{dst_data} = __riscv_vfmacc_vf_f32m1{dst_data}, {lhs_data}, {rhs_data},{vl});")
 def rvv_vfmacc_1xf32_4xf32(
@@ -184,6 +243,48 @@ def rvv_vfmacc_1xf32_4xf32(
 
     for i in seq(0, vl):
         dst[i] += lhs[0] * rhs[i]
+
+@instr("{dst_data} = __riscv_vfmacc_vf_f32m1{dst_data}, {lhs_data}, {rhs_data},{vl});")
+def rvv_vfmacc_1xf32_8xf32(
+    dst: [f32][8] @ RVV, lhs: [f32][1] @ DRAM, rhs: [f32][8] @ RVV, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] += lhs[0] * rhs[i]
+
+
+
+@instr("{dst_data} = __riscv_vrgather_vx_f32m1({src_data}, {imm}, {vl});")
+def rvv_gather_4xf32(dst: [f32][4] @ RVV, src: [f32][4] @ RVV, imm: index, vl: size):
+        assert stride(dst, 0) == 1
+        assert stride(src, 0) == 1
+        assert imm >= 0
+        assert imm < 4
+        assert vl >= 0
+        assert vl <= 4
+
+        for i in seq(0, vl):
+            dst[i] = src[imm]
+
+   
+@instr("{dst_data} = __riscv_vrgather_vx_f32m1({src_data}, {imm}, {vl});")
+def rvv_gather_8xf32(dst: [f32][8] @ RVV, src: [f32][8] @ RVV, imm: index, vl: size):
+        assert stride(dst, 0) == 1
+        assert stride(src, 0) == 1
+        assert imm >= 0
+        assert imm < 8
+        assert vl >= 0
+        assert vl <= 8
+
+        for i in seq(0, vl):
+            dst[i] = src[imm]
+
+
 
 # --------------------------------------------------------------------------- #
 #   f16 RVV intrinsics
@@ -205,6 +306,16 @@ def rvv_vld_8xf16(dst: [f16][8] @ RVV, src: [f16][8] @ DRAM, vl: size):
     for i in seq(0, vl):
         dst[i] = src[i]
 
+@instr("{dst_data} = __riscv_vle16_v_f16m1(&{src_data},{vl});")
+def rvv_vld_16xf16(dst: [f16][16] @ RVV, src: [f16][16] @ DRAM, vl: size):
+    assert stride(src, 0) == 1
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] = src[i]
+
 
 @instr("__riscv_vse16_v_f16m1(&{dst_data}, {src_data},{vl});")
 def rvv_vst_8xf16(dst: [f16][8] @ DRAM, src: [f16][8] @ RVV, vl: size):
@@ -217,11 +328,30 @@ def rvv_vst_8xf16(dst: [f16][8] @ DRAM, src: [f16][8] @ RVV, vl: size):
         dst[i] = src[i]
 
 
+@instr("__riscv_vse16_v_f16m1(&{dst_data}, {src_data},{vl});")
+def rvv_vst_16xf16(dst: [f16][16] @ DRAM, src: [f16][16] @ RVV, vl: size):
+    assert stride(src, 0) == 1
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] = src[i]
+
 @instr("{dst_data} = __riscv_vfmv_v_f_f16m1({src_data},{vl});")
 def rvv_broadcast_8xf16(dst: [f16][8] @ RVV, src: [f16][1] @ DRAM, vl: size):
     assert stride(dst, 0) == 1
     assert vl >= 0
     assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] = src[0]
+
+@instr("{dst_data} = __riscv_vfmv_v_f_f16m1({src_data},{vl});")
+def rvv_broadcast_16xf16(dst: [f16][16] @ RVV, src: [f16][1] @ DRAM, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
 
     for i in seq(0, vl):
         dst[i] = src[0]
@@ -236,12 +366,30 @@ def rvv_broadcast_8xf16_scalar(dst: [f16][8] @ RVV, src: f16 @ DRAM, vl: size):
     for i in seq(0, vl):
         dst[i] = src
 
+@instr("{dst_data} = __riscv_vfmv_v_f_f16m1({src_data},{vl});")
+def rvv_broadcast_16xf16_scalar(dst: [f16][16] @ RVV, src: f16 @ DRAM, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] = src
+
 
 @instr("{dst_data} = __riscv_vfmv_v_f_f16m1(0.0f,{vl});")
 def rvv_broadcast_8xf16_0(dst: [f16][8] @ RVV, vl: size):
     assert stride(dst, 0) == 1
     assert vl >= 0
     assert vl <= 8
+
+    for i in seq(0, vl):
+        dst[i] = 0.0
+
+@instr("{dst_data} = __riscv_vfmv_v_f_f16m1(0.0f,{vl});")
+def rvv_broadcast_16xf16_0(dst: [f16][16] @ RVV, vl: size):
+    assert stride(dst, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
 
     for i in seq(0, vl):
         dst[i] = 0.0
@@ -261,6 +409,20 @@ def rvv_vfmacc_8xf16_8xf16(
         dst[i] += lhs[i] * rhs[i]
 
 
+@instr("{dst_data} = __riscv_vfmacc_vv_f16m1({dst_data}, {lhs_data}, {rhs_data},{vl});")
+def rvv_vfmacc_16xf16_16xf16(
+    dst: [f16][16] @ RVV, lhs: [f16][16] @ RVV, rhs: [f16][16] @ RVV, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] += lhs[i] * rhs[i]
+
+
 @instr("{dst_data} = __riscv_vfmacc_vf_f16m1{dst_data}, {rhs_data}, {lhs_data},{vl});")
 def rvv_vfmacc_8xf16_1xf16(
     dst: [f16][8] @ RVV, lhs: [f16][8] @ RVV, rhs: [f16][1] @ DRAM, vl: size
@@ -275,6 +437,19 @@ def rvv_vfmacc_8xf16_1xf16(
         dst[i] += lhs[i] * rhs[0]
 
 
+@instr("{dst_data} = __riscv_vfmacc_vf_f16m1{dst_data}, {rhs_data}, {lhs_data},{vl});")
+def rvv_vfmacc_16xf16_1xf16(
+    dst: [f16][16] @ RVV, lhs: [f16][16] @ RVV, rhs: [f16][1] @ DRAM, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] += lhs[i] * rhs[0]
+
 @instr("{dst_data} = __riscv_vfmacc_vf_f16m1{dst_data}, {lhs_data}, {rhs_data},{vl});")
 def rvv_vfmacc_1xf16_8xf16(
     dst: [f16][8] @ RVV, lhs: [f16][1] @ DRAM, rhs: [f16][8] @ RVV, vl: size
@@ -287,3 +462,42 @@ def rvv_vfmacc_1xf16_8xf16(
 
     for i in seq(0, vl):
         dst[i] += lhs[0] * rhs[i]
+
+@instr("{dst_data} = __riscv_vfmacc_vf_f16m1{dst_data}, {lhs_data}, {rhs_data},{vl});")
+def rvv_vfmacc_1xf16_16xf16(
+    dst: [f16][16] @ RVV, lhs: [f16][1] @ DRAM, rhs: [f16][16] @ RVV, vl: size
+):
+    assert stride(dst, 0) == 1
+    assert stride(lhs, 0) == 1
+    assert stride(rhs, 0) == 1
+    assert vl >= 0
+    assert vl <= 16
+
+    for i in seq(0, vl):
+        dst[i] += lhs[0] * rhs[i]
+
+
+
+@instr("{dst_data} = __riscv_vrgather_vx_f16m1({src_data}, {imm}, {vl});")
+def rvv_gather_8xf16(dst: [f16][8] @ RVV, src: [f16][8] @ RVV, imm: index, vl: size):
+        assert stride(dst, 0) == 1
+        assert stride(src, 0) == 1
+        assert imm >= 0
+        assert imm < 8
+        assert vl >= 0
+        assert vl <= 8
+
+        for i in seq(0, vl):
+            dst[i] = src[imm]
+
+@instr("{dst_data} = __riscv_vrgather_vx_f16m1({src_data}, {imm}, {vl});")
+def rvv_gather_16xf16(dst: [f16][16] @ RVV, src: [f16][16] @ RVV, imm: index, vl: size):
+        assert stride(dst, 0) == 1
+        assert stride(src, 0) == 1
+        assert imm >= 0
+        assert imm < 16
+        assert vl >= 0
+        assert vl <= 16
+
+        for i in seq(0, vl):
+            dst[i] = src[imm]
