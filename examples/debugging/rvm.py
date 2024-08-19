5da0917e0662398d4a019ac4f5fca7524a21c92f
Yuka Ikarashi <yukatkh@gmail.com>
2024-08-19 18:55:00 -0400

    Rewrite RVM example

--- examples/debugging/rvm.py
+++ examples/debugging/rvm.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 import os
 import sys
 
-
+import exo.API_cursors as pc
 from exo import proc
 from exo.libs.memories import *
 from exo.platforms.x86 import *
 from exo.stdlib.scheduling import *
+from exo.stdlib.stdlib import *
 
 
 class RVM_TILE(StaticMemory):
@@ -64,7 +65,7 @@ def rvm_mzero(dst: [i32][4, 4] @ RVM_TILE):
 
     for i in seq(0, 4):
         for j in seq(0, 4):
-            dst[i, j] = 0
+            dst[i, j] = 0.0
 
 
 @instr(
@@ -117,145 +118,285 @@ def gen_conv1d():
         kernels: i32[K, C, R],
         out: i32[K, W],
     ):
-        # zero out the result memory
-        for k_init in seq(0, K):
-            for i_init in seq(0, W):
-                out[k_init, i_init] = 0.0
-
         # do the convolution
         for k in seq(0, K):
-            for c in seq(0, C):
-                for i in seq(0, W):
+            for i in seq(0, W):
+                # zero out the result memory
+                out[k, i] = 0.0
+                for c in seq(0, C):
                     for r in seq(0, R):
                         y: i32
-                        y = 0
                         if i + r < W:
                             y = data[c, i + r]
+                        else:
+                            y = 0
                         out[k, i] += kernels[k, c, r] * y
 
     return generic_conv1d
 
 
-def make_im2col_from_generic(p):
-    # Let's start applying scheduling
-    p = rename(p, "im2col_conv")
-    p = reorder_loops(p, "c i")
-    p = fuse(p, "for k_init in _:_", "for k in _:_")
-    p = fuse(p, "for i_init in _:_", "for i in _:_")
-    # add the tiles corresponding to the size of our systolic array
-    p = divide_loop(
-        p, "for k_init in _:_", TILE, new_iters=["tile_i", "i"], perfect=True
-    )
-    p = divide_loop(
-        p, "for i_init in _:_", TILE, new_iters=["tile_j", "j"], perfect=True
-    )
-    p = reorder_loops(p, "i tile_j")
-    # tile once again since we have 4 registers, we want to do 4 computes on different output channels at once
-    p = divide_loop(p, "for tile_i in _:_", 4, new_iters=["hi", "lo"], perfect=True)
-    p = reorder_loops(p, "lo tile_j")
+def fuse_two_loops(p, c):
+    """
+    for i in ...:         <- c
+        for j in ...:
+            s1
+    for k in ...:         <- c.next()
+        for i in ...:
+            s2
+    ---->
+    for i in ...:         <- c
+        for j in ...:
+            s1
+        for k in ...:
+            s2
+    """
+    try:
+        next_c = c.next()
+    except:
+        return p, False
 
-    # channels should be on the outside of each of these inner loops; these will all be part of the RVM instructions themselves
-    p = autofission(p, p.find("for c in _:_").before(), 3)
-    p = reorder_loops(p, "j c")
-    p = reorder_loops(p, "i c")
-    p = reorder_loops(p, "lo c")
+    if isinstance(c, pc.ForCursor) and isinstance(next_c, pc.ForCursor):
+        if c.name() == next_c.name() and expr_to_string(c.hi()) == expr_to_string(
+            next_c.hi()
+        ):
+            p = fuse(p, c, next_c, unsafe_disable_check=False)
+            return p, True
+        else:
+            tgt_c, count = find_child_loop(next_c, c.name())
+            if tgt_c:
+                p = lift_scope_n(p, tgt_c, n_lifts=count)
+                p = fuse(p, c, tgt_c, unsafe_disable_check=False)
+                return p, True
 
-    # next, we can start to lift that allocation
-    # up and out of the loop
-    p = autolift_alloc(p, "y:i32", 4, keep_dims=True)
-    p = set_memory(p, "y: _", DRAM_STATIC)
+    return p, False
 
-    # Then, we can fission the loop correspondingly,
-    # separating what is now a data-marshalling statement from
-    # the actual compute statement in two subsequent
-    # loop nests via fissioning
-    p = autofission(p, p.find("out[_] += _").before(), 4)
-    p = simplify(p)
+
+def fuse_all_loops(p, cursor):
+    """
+    recursively calls fuse_two_loops to all the loops
+    """
+    while True:
+        if isinstance(cursor, pc.ForCursor):
+            p = fuse_all_loops(p, cursor.body()[0])
+
+        # Fuse in current scope
+        p, b = fuse_two_loops(p, cursor)
+
+        if b:
+            cursor = p.forward(cursor)
+        else:
+            try:
+                cursor = p.forward(cursor).next()
+            except:
+                break
+
     return p
 
 
-def rvm_optimize(p):
-    # Setting up data tile load
-    p = stage_mem(p, "for lo in _:_ #1", f"y[0:{TILE}, 0:{TILE}]", "data_tile")
-    p = set_memory(p, "data_tile", RVM_TILE)
-    p = replace(p, "for i0 in _:_", rvm_mld)
+def autolift_alloc(p, alloc_c, dep_set=None, max_size=0, lift=True):
+    """
+    for i in seq(0, 10):
+        for j in seq(0, 20):
+            a : R          <- alloc_c, dep_set = {'i'}
+            a[i] = ...
+    ---->
+    a : R[10]              <- if size is less than max_size
+    for i in seq(0, n):
+        for j in seq(0, m):
+            a[i] = ...
+    """
+    alloc_c = p.forward(alloc_c)
+    loop_c = get_enclosing_loop(p, alloc_c)
+    accum_size = 1
+    while True:
+        try:
+            if not isinstance(loop_c, pc.ForCursor):
+                break
+            if dep_set == None or loop_c.name() in dep_set:
+                if (
+                    isinstance(loop_c.hi(), LiteralCursor)
+                    and accum_size * loop_c.hi().value() <= max_size
+                ):
+                    p = expand_dim(p, alloc_c, loop_c.hi().value(), loop_c.name())
+                    accum_size = accum_size * loop_c.hi().value()
+                    if lift:
+                        p = lift_alloc(p, alloc_c)
+            loop_c = loop_c.parent()
+        except:
+            break
+    return p
 
-    # Setting up kernel tile load
-    p = stage_mem(
-        p,
-        "for i in _:_ #1",
-        f"kernels[hi*{TILE}*4 + 4*lo:hi*{TILE}*4 + 4*lo+{TILE}, c, 0:{TILE}]",
-        "kernel_tile",
+
+def reorder_top(p, c):
+    """
+    for i in seq(0, 10):
+        s1
+        s2
+        s3  <- c
+    ---->
+    for i in seq(0, 10):
+        s3  <- c
+        s1
+        s2
+    """
+    c = p.forward(c)
+    while True:
+        try:
+            p = reorder_stmts(p, c.expand(1, 0))
+            c = p.forward(c)
+        except:
+            break
+    return p
+
+
+def fission_as_much_as_possible(p, cursor):
+    """
+    for i in ...:
+        for j in ...:
+            s1
+            s2        <- cursor
+            s3
+    --->
+    for i in ...:
+        for j in ...:
+            s2
+
+    for i in ...:
+        for j in ...:
+            s1
+            s3
+    """
+    cursor = p.forward(cursor)
+    p = reorder_top(p, cursor)
+    gap_c = cursor.after()
+    while True:
+        try:
+            p = fission(p, gap_c)
+            gap_c = p.forward(gap_c).parent().after()
+        except:
+            break
+
+    return p
+
+
+def lift_scope_n(p, c, n_lifts=1):
+    """
+    for i in seq(0, 10):
+        for j in seq(0, 10):
+            for k in seq(0, 10):
+                if ...:  <- c
+                    s1
+    ----> if n_lifts == 2:
+    for i in seq(0, 10):
+        if ...:  <- c
+            for j in seq(0, 10):
+                for k in seq(0, 10):
+                    s1
+    """
+    for i in range(0, n_lifts):
+        p = lift_scope(p, c)
+    return p
+
+
+def remove_redundant_loops(p, c, num=0):
+    """
+    for i in ...:
+        for j in ...:
+            s1[j]      <- c
+    --->
+    for j in ...:
+        s1[j]          <- c
+    """
+    c = p.forward(c)
+    cur_depth = 0
+    while True:
+        c = c.parent()
+        if not isinstance(c, pc.ForCursor):
+            break
+        try:
+            if cur_depth >= num:
+                break
+            hi = c.hi().value()
+            name = c.name()
+            child = p.forward(c).body()[0]
+            p = remove_loop(p, c)
+            cur_depth += 1
+        except:
+            continue
+    return p
+
+
+def optimize_conv(p):
+    p = rename(p, "exo_conv1d_tile_lt_kw")
+
+    # Before scheduling, grab cursors to the object code.
+    k_loop = p.find("for k in _:_")
+    i_loop = p.find("for i in _:_")
+    c_loop = p.find("for c in _:_")
+    y_alloc = p.find("y : _")
+    y_assign = p.find("y = data[_]")
+
+    # Tile outer loops to TILE size for RVM
+    p, _ = tile_loops(p, [(k_loop, TILE), (i_loop, TILE)], perfect=True)
+    p = reorder_loops(p, k_loop)
+
+    # Stage output to out_tile
+    p, (_, out_tile, body, _) = auto_stage_mem(
+        p, p.find_loop("c").expand(1, 0), "out", "out_tile", rc=True
     )
-    p = set_memory(p, "kernel_tile", RVM_TILE)
-    p = replace(p, "for i0 in _:_", rvm_mld)
+    p = autolift_alloc(p, out_tile, max_size=4 * 4 * 4)
 
-    # Setting up output tile
-    # Here we are specifiying all 4 output registers at once.
-    p = stage_mem(
-        p,
-        "for c in _:_",
-        f"out[hi*{TILE}*4: hi*{TILE}*4+{TILE}*4, tile_j*{TILE}:tile_j*{TILE}+{TILE}]",
-        "output_tile",
+    # Block the zero initialization and store blocks
+    p = fission_as_much_as_possible(p, body)
+    p = fission_as_much_as_possible(p, body[0])
+
+    # Reorder c loop to the top
+    p = lift_scope_n(p, c_loop, 3)
+
+    # Stage y
+    p = autolift_alloc(p, y_alloc, max_size=4 * 4)
+    p = lift_alloc(p, y_alloc, n_lifts=2)
+
+    # Fission the initialization loop and remove redundant loops
+    p = fission_as_much_as_possible(p, y_assign.parent())
+    p = remove_redundant_loops(p, y_assign.parent(), num=2)
+
+    # Stage kernels to kernel_tile and y to data_tile
+    ki_loop = p.forward(c_loop).body()[2].body()[0]
+    p, (kernel_alloc, _, _, _) = auto_stage_mem(
+        p, ki_loop, "kernels", "kernel_tile", rc=True
     )
-    # That means we need to divide the buffers further.
-    p = set_memory(p, "output_tile", RVM_TILE)
-    p = divide_dim(p, "output_tile:_", 0, 4)
+    p = simplify(expand_dim(p, kernel_alloc, 4, ki_loop.parent().name()))
+    p = lift_alloc(p, kernel_alloc)
+    p, (data_alloc, _, _, _) = auto_stage_mem(
+        p, ki_loop.parent(), "y", "data_tile", rc=True
+    )
 
-    # In addition, the loops which do the clearing and storing of this buffer should be tiled,
-    # that way the inner nest can be replaced with the appropriate matrix instructions.
-    p = divide_loop(p, "for i0 in _:_", 4, ["d_lo", "d_i"], perfect=True)
-    p = divide_loop(p, "for i0 in _:_", 4, ["s_lo", "s_i"], perfect=True)
+    # Set adequate memories
+    p = set_memory(p, y_alloc, DRAM_STATIC)
+    p = set_memory(p, out_tile, RVM_TILE)
+    p = set_memory(p, kernel_alloc, RVM_TILE)
+    p = set_memory(p, data_alloc, RVM_TILE)
 
-    # Fuse output zeroing loop with generated stage_mem loop,
-    # that way we can replace to mzero
+    # Replace inner loops to calls to RVM instructions
+    p = replace_all(p, [rvm_mzero, rvm_mst, rvm_mld, rvm_mmasa])
+
+    # Clean up
+    p = unroll_loop(p, "ko")
+    p = unroll_loop(p, "ko")
+    p = unroll_loop(p, "ko")
     p = simplify(p)
-    # loops need to be adjacent, get this alloc out of the way
-    # need to simplify first otherwise it's dependent on the index
-    p = lift_alloc(p, "output_tile: _")
-    p = fuse(p, "for lo in _:_ #0", "for d_lo in _:_ #0")
-    p = fuse(p, "for i in _:_ #0", "for d_i in _:_ #0")
-    p = fuse(p, "for j in _:_ #0", "for i1 in _:_")
-    p = sink_alloc(p, "output_tile: _")
-
-    # remove the assignment to out[]
-    # TODO: Not a bug, but it's interesting that this is correct?
-    # Exo must be able to prove that I don't read from out[] later on in the program..
-    p = inline_assign(p, "out[i + 4 * lo + 16 * hi, j + 4 * tile_j] = 0")
-    p = replace(p, "for i in _:_ #0", rvm_mzero)
-
-    # Setting up output tile store
-    p = replace(p, "for s_i in _:_ ", rvm_mst)
-
-    # Replace with matmul instruction
-    p = replace(p, "for i in _:_", rvm_mmasa)
-
-    # unroll everything!
-    p = unroll_loop(p, "for lo in _:_")
-    p = unroll_loop(p, "for s_lo in _:_")
-    p = simplify(p)
-
-    # some gymnastics for reusing the output of the load so we don't exceed the 8 available registers
-    p = autolift_alloc(p, "kernel_tile: _", keep_dims=True)
-    p = unroll_loop(p, "for lo in _:_")
-    p = unroll_buffer(p, "kernel_tile: _", 0)
-    p = reorder_stmts(p, "kernel_tile_3: _; rvm_mld(_)")
+    p = unroll_buffer(p, kernel_alloc, 0)
     p = reuse_buffer(p, "kernel_tile_0: _", "kernel_tile_3: _")
 
-    p = unroll_buffer(p, "output_tile: _", 0)
+    print(p)
 
-    p = simplify(p)
-
-    # Done!
-    p = simplify(p)
-    p = rename(p, "exo_conv1d_tile_lt_kw")
     return p
 
 
 def make_routine():
     generic_conv1d = gen_conv1d()
-    im2col_cpu = make_im2col_from_generic(generic_conv1d)
-    rvm_optimized = rvm_optimize(im2col_cpu)
+    rvm_optimized = optimize_conv(generic_conv1d)
     return rvm_optimized
 
 
