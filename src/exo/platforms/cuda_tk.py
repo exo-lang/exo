aff107ecfb272bbb86562a41f8aeb7c48fd37cf7
David Zhao Akeley <dza724@gmail.com>
2026-03-19 10:52:47 -0700

    Add comments to cuda_tk.py

--- src/exo/platforms/cuda_tk.py
+++ src/exo/platforms/cuda_tk.py
@@ -1,16 +1,44 @@
+# fmt: off
+"""Top-level module for Exo-GPU wrappers of ThunderKittens
+
+We have focused on wrapping tile ops and tile-to-vec reductions.
+Vector support is limited but could be added.
+
+For register (RMEM) storage, we wrap the ThunderKittens tile and vec types
+as Exo-GPU Memory classes. Use CudaTkWarpTile(rows, cols, layout="row")
+and its row_vec and col_vec attributes. These are allocated at warp scope.
+
+For SMEM and GMEM, we continue to use Exo-GPU's low-level memory classes,
+for easy inter-operation with non-ThunderKittens code:
+
+    CudaGmemLinear, for GMEM
+    Sm90_SmemSwizzled(swizzle), for SMEM tiles (usually swizzle=128)
+    CudaSmemLinear, for SMEM vectors
+
+The instructions have to do a bunch of C++ casts to make this work.
+
+WGMMA and TMA are not exposed here. Use the Sm90 module.
+Sm90_tk_ instructions use ThunderKittens RMEM tiles. TMA is completely independent.
+In particular, we do NOT use kittens::GL and its automatic tensorMap dict.
+This is a smart abstraction for C++, but too high-level to adapt cleanly to Exo.
+
+"""
+
+# fmt: on
+
 # Register tile and vector kittens types
 from .kittens_impl.tk_types import *
 
-# Register-to-register tile conversion, maps, reductions
+# Register-to-register tile copy, conversion, maps, reduction-to-vector
 from .kittens_impl.tk_tile_ops import *
 
-# RMEM/SMEM instructions
+# RMEM/SMEM tile copies
 from .kittens_impl.tk_shared_to_register import *
 
-# RMEM/GMEM instructions
+# RMEM/GMEM tile copies
 from .kittens_impl.tk_global_to_register import *
 
-# SMEM/GMEM instructions
+# SMEM/GMEM tile copies
 from .kittens_impl.tk_global_to_shared import *
 
 
