a0222a268714ae3d84f14744dcfa63e536dccc48
David Zhao Akeley <dza724@gmail.com>
2026-04-29 00:25:46 -0400

    Update test_Sm90a_gemm for hamster mbarrier

--- tests/cuda/test_Sm90a_gemm.py
+++ tests/cuda/test_Sm90a_gemm.py
@@ -13,30 +13,37 @@ from exo.platforms.Sm90 import *
 
 from exo.core.LoopIR import get_global_debug_log
 
-# TODO find a better home for these than exo.platforms
-# For now they're there in order to piggyback off exo's package structure.
-# I don't have patience to deal with "attempted relative input without XXX" BS.
-from exo.platforms.gemm_Sm90a_test.make_Sm90a_gemm import make_Sm90a_gemm
-from exo.platforms.gemm_Sm90a_test.schedule_Sm90a_gemm import schedule_Sm90a_gemm
-from exo.platforms.gemm_Sm90a_test.Sm90a_gemm_pre_config import Sm90aGemmConfig
-
-
-config_A = Sm90aGemmConfig(
-    smem_M=128, smem_N=256, tma_to_gmem=False, enable_split_k=False
-)
-config_B = Sm90aGemmConfig(
-    smem_M=256, smem_N=192, tma_to_gmem=False, enable_split_k=False
-)
-config_K = Sm90aGemmConfig(
-    smem_M=256, smem_N=192, tma_to_gmem=True, enable_split_k=True
+import exo.platforms.Sm90.tk_gemm_util as gemm_util
+from exo.platforms.Sm90.tk_gemm_util import (
+    L_divisor,
+    M_divisor,
+    N_divisor,
+    K_cluster_divisor,
 )
 
+from dataclasses import replace  # Name conflict with exo.stdlib.scheduling...
 
-def mkproc_gemm(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled):
+
+f32_config = gemm_util.GemmConfig(
+    A_type="f32",
+    A_major="row",
+    B_type="f32",
+    B_major="col",
+    C_type="f32",
+    C_major="row",
+)
+
+config_A = replace(f32_config, cta_M=128, cta_N=256, enable_split_k=False)
+config_B = replace(f32_config, cta_M=256, cta_N=192, enable_split_k=False)
+config_K = replace(f32_config, cta_M=256, cta_N=192, enable_split_k=True)
+
+
+def mkproc_gemm(config: gemm_util.GemmConfig, ncta_M, ncta_N, scheduled):
+    config = replace(config, ncta_M=ncta_M, ncta_N=ncta_N)
     if scheduled:
-        gpu_gemm = schedule_Sm90a_gemm(config, ncta_M, ncta_N)
+        assert 0, "TODO schedule"
     else:
-        gpu_gemm = make_Sm90a_gemm(config, ncta_M, ncta_N)
+        gpu_gemm = gemm_util.handwrite_gemm(config)
 
     @proc
     def cpu_gemm_wrapper(
@@ -44,29 +51,32 @@ def mkproc_gemm(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled):
         M: size,
         N: size,
         K_split: size,
-        cluster_K: size,
-        A: f32[L, M, K_split, cluster_K] @ DRAM,
-        B: f32[L, N, K_split, cluster_K] @ DRAM,
-        C: f32[L, N, M] @ DRAM,
+        K_cluster: size,
+        A: f32[L, M, K_split, K_cluster] @ DRAM,
+        B: f32[L, N, K_split, K_cluster] @ DRAM,
+        C: f32[L, M, N] @ DRAM,
     ):
-        assert cluster_K % 4 == 0
-        d_A: f32[L, M, K_split, cluster_K] @ CudaGmemLinear
-        d_B: f32[L, N, K_split, cluster_K] @ CudaGmemLinear
-        d_C: f32[L, N, M] @ CudaGmemLinear
+        assert L % L_divisor == 0
+        assert M % M_divisor == 0
+        assert N % N_divisor == 0
+        assert K_cluster % K_cluster_divisor == 0
+        d_A: f32[L, M, K_split, K_cluster] @ CudaGmemLinear
+        d_B: f32[L, N, K_split, K_cluster] @ CudaGmemLinear
+        d_C: f32[L, M, N] @ CudaGmemLinear
 
         cudaMemcpyAsync_htod_4f32(
-            L, M, K_split, cluster_K, d_A[:, :, :, :], A[:, :, :, :]
+            L, M, K_split, K_cluster, d_A[:, :, :, :], A[:, :, :, :]
         )
         cudaMemcpyAsync_htod_4f32(
-            L, N, K_split, cluster_K, d_B[:, :, :, :], B[:, :, :, :]
+            L, N, K_split, K_cluster, d_B[:, :, :, :], B[:, :, :, :]
         )
-        gpu_gemm(L, M, N, K_split, cluster_K, d_A, d_B, d_C)
-        cudaMemcpyAsync_dtoh_3f32(L, N, M, C[:, :, :], d_C[:, :, :])
+        gpu_gemm(L, M, N, K_split, K_cluster, d_A, d_B, d_C)
+        cudaMemcpyAsync_dtoh_3f32(L, M, N, C[:, :, :], d_C[:, :, :])
 
     return cpu_gemm_wrapper
 
 
-def mktest_golden(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled):
+def mktest_golden(config: gemm_util.GemmConfig, ncta_M, ncta_N, scheduled):
     def test(compiler, golden):
         compiler.cuda_cpu_test(
             mkproc_gemm,
@@ -80,19 +90,19 @@ def mktest_golden(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled):
     return test
 
 
-def mktest_run(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled, K_split):
+def mktest_run(config: gemm_util.GemmConfig, ncta_M, ncta_N, scheduled, K_split):
     # More thorough runtime testing will remain in sporkbench.
     L = 1
     M = 2000
     N = 2004
     K = 1600
-    cluster_K = K // K_split
+    K_cluster = K // K_split
 
     def test(compiler_Sm90a):
         rand = random.Random(100)
         A = np.ndarray(shape=(M, K), dtype=np.float32, order="C")
         B = np.ndarray(shape=(K, N), dtype=np.float32, order="F")
-        C = np.ndarray(shape=(M, N), dtype=np.float32, order="F")
+        C = np.ndarray(shape=(M, N), dtype=np.float32, order="C")
 
         for m in range(0, M):
             for k in range(0, K):
@@ -106,7 +116,7 @@ def mktest_run(config: Sm90aGemmConfig, ncta_M, ncta_N, scheduled, K_split):
         )
 
         C_expected = A @ B
-        cu(None, L, M, N, K_split, cluster_K, A, B, C)
+        cu(None, L, M, N, K_split, K_cluster, A, B, C)
         assert np.array_equal(C, C_expected)
 
     return test
@@ -121,8 +131,8 @@ test_golden_Sm90a_gemm_m1n2_B = mktest_golden(config_B, 1, 2, False)
 test_golden_Sm90a_gemm_m2n2_K = mktest_golden(config_K, 2, 2, False)
 test_run_Sm90a_gemm_m2n2_K = mktest_run(config_K, 2, 2, False, K_split=2)
 
-test_golden_Sm90a_sch_gemm_m1n2_B = mktest_golden(config_B, 1, 2, True)
-test_run_Sm90a_sch_gemm_m1n2_B = mktest_run(config_B, 1, 2, True, K_split=1)
+# test_golden_Sm90a_sch_gemm_m1n2_B = mktest_golden(config_B, 1, 2, True)
+# test_run_Sm90a_sch_gemm_m1n2_B = mktest_run(config_B, 1, 2, True, K_split=1)
 
 
 def test_Sm90a_gemm_remarks(compiler, golden):
@@ -131,7 +141,8 @@ def test_Sm90a_gemm_remarks(compiler, golden):
     # We just want to force some logging code coverage.
 
     def mkproc():
-        gpu_gemm = make_Sm90a_gemm(config_K, 2, 1)
+        config = replace(config_K, ncta_M=2, ncta_N=1)
+        gpu_gemm = gemm_util.handwrite_gemm(config)
         gpu_gemm = rename(gpu_gemm, "gpu_gemm")
         return gpu_gemm
 
